f5416a2fcef296c2faa9dd9097480770beb0b45f
Daniel Abraham <daniel@autokitteh.com>
2024-02-20 10:27:51 -0800

    Remove daemon flag from up command

--- cmd/ak/cmd/up.go
+++ cmd/ak/cmd/up.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -12,23 +11,15 @@ import (
 	"go.autokitteh.dev/autokitteh/cmd/ak/common"
 )
 
-var (
-	daemon bool
-	mode   string
-)
+var mode string
 
 var upCmd = common.StandardCommand(&cobra.Command{
-	Use:     "up [--mode={default|dev|test}] [--daemon]",
+	Use:     "up [--mode={default|dev|test}]",
 	Short:   "Start local server",
 	Aliases: []string{"u"},
 	Args:    cobra.NoArgs,
 
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if daemon {
-			// TODO: Implement. Need to detach only after all inits are done?
-			return errors.New("not implemented yet")
-		}
-
 		m, err := configset.ParseMode(mode)
 		if err != nil {
 			return fmt.Errorf("mode: %w", err)
@@ -40,10 +31,7 @@ var upCmd = common.StandardCommand(&cobra.Command{
 	},
 })
 
-func RemoveUpCmd() { RootCmd.RemoveCommand(upCmd) }
-
 func init() {
 	// Command-specific flags.
 	upCmd.Flags().StringVarP(&mode, "mode", "m", "", "run mode: {default|dev|test}")
-	upCmd.Flags().BoolVarP(&daemon, "daemon", "d", false, "run as daemon?")
 }
