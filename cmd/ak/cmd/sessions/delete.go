36e4d6f62f3065c28d49600c37778d4d360cdf24
Igor Rayak <igor@autokitteh.com>
2024-02-21 20:14:41 +0200

    add delete session cmd

---
+++ cmd/ak/cmd/sessions/delete.go
@@ -0,0 +1,58 @@
+package sessions
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"go.autokitteh.dev/autokitteh/cmd/ak/common"
+	"go.autokitteh.dev/autokitteh/internal/kittehs"
+	"go.autokitteh.dev/autokitteh/internal/resolver"
+)
+
+var deleteCmd = common.StandardCommand(&cobra.Command{
+	Use:     "delete [session ID] [--fail]",
+	Short:   "Delete non-running session",
+	Aliases: []string{"d"},
+	Args:    cobra.MaximumNArgs(1),
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			id, err := latestSessionID()
+			if err != nil {
+				return err
+			}
+			args = append(args, id)
+		}
+
+		r := resolver.Resolver{Client: common.Client()}
+		s, id, err := r.SessionID(args[0])
+		if err != nil {
+			return err
+		}
+
+		if s != nil {
+			ctx, cancel := common.LimitedContext()
+			defer cancel()
+
+			common.RenderKVIfV("session", s) // FIXME: should we print deleted session?
+			err = sessions().Delete(ctx, id)
+			if err != nil {
+				err = fmt.Errorf("delete session: %w", err)
+			}
+		} else {
+			err = common.NewExitCodeError(common.NotFoundExitCode, fmt.Errorf("session ID %q not found", id))
+		}
+
+		if kittehs.Must1(cmd.Flags().GetBool("fail")) && err != nil {
+			return err
+		}
+
+		return nil
+	},
+})
+
+func init() {
+	// Command-specific flags.
+	common.AddFailIfNotFoundFlag(deleteCmd)
+}
