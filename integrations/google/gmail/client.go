a2f20ec35160e11b3ffc0f159c95611828f3c087
Daniel Abraham <daniel@autokitteh.com>
2024-02-27 20:36:45 -0800

    Fix access to Google secrets for product-specific integrations

--- integrations/google/gmail/client.go
+++ integrations/google/gmail/client.go
@@ -41,7 +41,7 @@ var desc = kittehs.Must1(sdktypes.StrictIntegrationFromProto(&sdktypes.Integrati
 }))
 
 func New(sec sdkservices.Secrets) sdkservices.Integration {
-	scope := sdktypes.GetIntegrationUniqueName(desc).String()
+	scope := "google"
 
 	opts := []sdkmodule.Optfn{sdkmodule.WithConfigAsData()}
 	opts = append(opts, ExportedFunctions(sec, scope, false)...)
