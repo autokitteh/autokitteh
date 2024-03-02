8bb8baf5461ad8b515a0de7b3819caa31baa82ba
Igor Rayak <igor@autokitteh.com>
2024-03-02 10:28:11 +0200

    add deployment delete cmd

---
+++ cmd/ak/cmd/deployments/delete.go
@@ -0,0 +1,42 @@
+package deployments
+
+import (
+	"github.com/spf13/cobra"
+
+	"go.autokitteh.dev/autokitteh/cmd/ak/common"
+	"go.autokitteh.dev/autokitteh/internal/resolver"
+)
+
+var deleteCmd = common.StandardCommand(&cobra.Command{
+	Use:     "delete <deployment ID> [--fail]",
+	Short:   "Delete inactive deployment",
+	Aliases: []string{"d"},
+	Args:    cobra.ExactArgs(1),
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		r := resolver.Resolver{Client: common.Client()}
+		d, id, err := r.DeploymentID(args[0])
+		if err != nil {
+			return common.FailIfError(cmd, err, "deployment")
+		}
+
+		if err := common.FailIfNotFound(cmd, "deployment id", d); err != nil {
+			return err
+		}
+
+		ctx, cancel := common.LimitedContext()
+		defer cancel()
+		err = deployments().Delete(ctx, id)
+		if err != nil {
+			return common.FailIfError(cmd, err, "session")
+		}
+
+		common.RenderKVIfV("deployment", d) // print deleted deployment
+		return nil
+	},
+})
+
+func init() {
+	// Command-specific flags.
+	common.AddFailIfError(deleteCmd)
+}
