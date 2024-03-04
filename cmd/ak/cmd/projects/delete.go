7d854206ac656d078a791ecc77771988c5a780c7
Igor Rayak <igor@autokitteh.com>
2024-03-04 13:16:54 +0200

    add project cmd

---
+++ cmd/ak/cmd/projects/delete.go
@@ -0,0 +1,40 @@
+package projects
+
+import (
+	"github.com/spf13/cobra"
+
+	"go.autokitteh.dev/autokitteh/cmd/ak/common"
+	"go.autokitteh.dev/autokitteh/internal/resolver"
+)
+
+var deleteCmd = common.StandardCommand(&cobra.Command{
+	Use:     "delete <project ID> [--fail]",
+	Short:   "Delete inactive project",
+	Aliases: []string{"d"},
+	Args:    cobra.ExactArgs(1),
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		r := resolver.Resolver{Client: common.Client()}
+		p, id, err := r.ProjectNameOrID(args[0])
+		if err != nil {
+			return common.FailIfError(cmd, err, "project")
+		}
+
+		if err := common.FailIfNotFound(cmd, "project", p); err != nil { // test p != nil
+			return err
+		}
+
+		ctx, cancel := common.LimitedContext()
+		defer cancel()
+		err = projects().Delete(ctx, id)
+		if err != nil { // report any other than "not found" error (which was handled above)
+			return common.ToExitCodeError(err, "delete project")
+		}
+		return nil
+	},
+})
+
+func init() {
+	// Command-specific flags.
+	common.AddFailIfError(deleteCmd)
+}
