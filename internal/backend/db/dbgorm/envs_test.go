dd6e87da69813849958f7aea7a8cebda0089a8cf
Igor Rayak <igor@autokitteh.com>
2024-03-08 10:00:13 +0200

    tests for env foreign keys

--- internal/backend/db/dbgorm/envs_test.go
+++ internal/backend/db/dbgorm/envs_test.go
@@ -13,6 +13,10 @@ func createEnvAndAssert(t *testing.T, f *dbFixture, env scheme.Env) {
 	findAndAssertOne(t, f, env, "env_id = ?", env.EnvID)
 }
 
+func assertEnvDeleted(t *testing.T, f *dbFixture, envID string) {
+	assertSoftDeleted(t, f, scheme.Env{EnvID: envID})
+}
+
 func TestCreateEnv(t *testing.T) {
 	f := newDbFixture(true)                       // no foreign keys
 	findAndAssertCount(t, f, scheme.Env{}, 0, "") // no envs
@@ -32,4 +36,30 @@ func TestDeleteEnv(t *testing.T) {
 	// test deleteEnv
 	assert.NoError(t, f.gormdb.deleteEnv(f.ctx, e.EnvID))
 	findAndAssertCount(t, f, scheme.Env{}, 0, "") // no envs
+}
+
+func TestDeleteEnvForeignKeys(t *testing.T) {
+	f := newDbFixture(false)
+	findAndAssertCount(t, f, scheme.Env{}, 0, "") // no envs
+
+	b := newBuild()
+	p := newProject()
+	e := newEnv()
+	e.ProjectID = p.ProjectID
+	d := newDeployment(b.BuildID, e.EnvID)
+
+	saveBuildAndAssert(t, f, b)
+	createProjectAndAssert(t, f, p)
+	createEnvAndAssert(t, f, e)
+	createDeploymentAndAssert(t, f, d)
+
+	// cannot delete env, since deployment referencing it
+	err := f.gormdb.deleteEnv(f.ctx, e.EnvID)
+	assert.ErrorContains(t, err, "FOREIGN KEY")
+
+	// delete deployment (referencing build), then build , then env
+	assert.NoError(t, f.gormdb.deleteDeployment(f.ctx, d.DeploymentID))
+	assert.NoError(t, f.gormdb.deleteBuild(f.ctx, b.BuildID))
+	assert.NoError(t, f.gormdb.deleteEnv(f.ctx, e.EnvID))
+	assertEnvDeleted(t, f, e.EnvID)
 }
