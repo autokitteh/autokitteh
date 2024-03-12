ce5ce4ca0468cac64b35e6f27109c52f18ab9d97
Igor Rayak <igor@autokitteh.com>
2024-03-12 23:06:46 +0200

    rename

--- internal/backend/db/dbgorm/secrets_test.go
+++ internal/backend/db/dbgorm/secrets_test.go
@@ -8,51 +8,51 @@ import (
 )
 
 func TestSetGetSecret(t *testing.T) {
-	db := newDBFixture(true) // No foreign keys.
+	f := newDBFixture(true) // No foreign keys.
 
 	want := map[string]string{"key": "value"}
-	err := db.gormdb.SetSecret(db.ctx, "name", want)
+	err := f.gormdb.SetSecret(f.ctx, "name", want)
 	assert.NoError(t, err)
 
-	got, err := db.gormdb.GetSecret(db.ctx, "name")
+	got, err := f.gormdb.GetSecret(f.ctx, "name")
 	assert.NoError(t, err)
 	assert.Equal(t, want, got)
 }
 
 func TestGetSecretError(t *testing.T) {
-	db := newDBFixture(true) // No foreign keys.
+	f := newDBFixture(true) // No foreign keys.
 
-	got, err := db.gormdb.GetSecret(db.ctx, "name")
+	got, err := f.gormdb.GetSecret(f.ctx, "name")
 	assert.ErrorIs(t, err, sdkerrors.ErrNotFound)
 	assert.Nil(t, got)
 }
 
 func TestSetAppendGetSecret(t *testing.T) {
-	db := newDBFixture(true) // No foreign keys.
+	f := newDBFixture(true) // No foreign keys.
 
 	data := map[string]string{"key1": "value1"}
-	err := db.gormdb.SetSecret(db.ctx, "name", data)
+	err := f.gormdb.SetSecret(f.ctx, "name", data)
 	assert.NoError(t, err)
 
-	err = db.gormdb.AppendSecret(db.ctx, "name", "key2")
+	err = f.gormdb.AppendSecret(f.ctx, "name", "key2")
 	assert.NoError(t, err)
 
-	got, err := db.gormdb.GetSecret(db.ctx, "name")
+	got, err := f.gormdb.GetSecret(f.ctx, "name")
 	assert.NoError(t, err)
 	assert.NotEqual(t, "", got["key2"])
 }
 
 func TestSetDeleteGetSecret(t *testing.T) {
-	db := newDBFixture(true) // No foreign keys.
+	f := newDBFixture(true) // No foreign keys.
 
 	data := map[string]string{"key": "value"}
-	err := db.gormdb.SetSecret(db.ctx, "name", data)
+	err := f.gormdb.SetSecret(f.ctx, "name", data)
 	assert.NoError(t, err)
 
-	err = db.gormdb.DeleteSecret(db.ctx, "name")
+	err = f.gormdb.DeleteSecret(f.ctx, "name")
 	assert.NoError(t, err)
 
-	got, err := db.gormdb.GetSecret(db.ctx, "name")
+	got, err := f.gormdb.GetSecret(f.ctx, "name")
 	assert.ErrorIs(t, err, sdkerrors.ErrNotFound)
 	assert.Nil(t, got)
 }
