9c8c1fa62895652326bc3954e405700879e070b6
Igor Rayak <igor@autokitteh.com>
2024-03-12 14:56:33 +0200

    tests for connections/triggers/integrations

---
+++ internal/backend/db/dbgorm/connections_test.go
@@ -0,0 +1,25 @@
+package dbgorm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.autokitteh.dev/autokitteh/internal/backend/db/dbgorm/scheme"
+)
+
+func createConnectionsAndAssert(t *testing.T, f *dbFixture, connections ...scheme.Connection) {
+	for _, conn := range connections {
+		assert.NoError(t, f.gormdb.createConnection(f.ctx, conn))
+		findAndAssertOne(t, f, conn, "connection_id = ?", conn.ConnectionID)
+	}
+}
+
+func TestCreateConnection(t *testing.T) {
+	f := newDbFixture(true)                              // no foreign keys
+	findAndAssertCount(t, f, scheme.Connection{}, 0, "") // no connections
+
+	tr := newConnection()
+	// test createConnection
+	createConnectionsAndAssert(t, f, tr)
+}
