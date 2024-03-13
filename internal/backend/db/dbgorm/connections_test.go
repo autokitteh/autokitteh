0d2e75d2230d06e7d6464458f648f568075e3190
Igor Rayak <igor@autokitteh.com>
2024-03-13 13:17:02 +0200

    connection delete test

--- internal/backend/db/dbgorm/connections_test.go
+++ internal/backend/db/dbgorm/connections_test.go
@@ -15,6 +15,12 @@ func (f *dbFixture) createConnectionsAndAssert(t *testing.T, connections ...sche
 	}
 }
 
+func (f *dbFixture) assertConnectionDeleted(t *testing.T, connections ...scheme.Connection) {
+	for _, connection := range connections {
+		assertDeleted(t, f, connection)
+	}
+}
+
 func TestCreateConnection(t *testing.T) {
 	f := newDBFixture(true)                              // no foreign keys
 	findAndAssertCount(t, f, scheme.Connection{}, 0, "") // no connections
@@ -22,4 +28,16 @@ func TestCreateConnection(t *testing.T) {
 	tr := f.newConnection()
 	// test createConnection
 	f.createConnectionsAndAssert(t, tr)
+}
+
+func TestDeleteConnection(t *testing.T) {
+	f := newDBFixture(true)                              // no foreign keys
+	findAndAssertCount(t, f, scheme.Connection{}, 0, "") // no connections
+
+	c := f.newConnection()
+	f.createConnectionsAndAssert(t, c)
+
+	// test deleteConnection
+	assert.NoError(t, f.gormdb.deleteConnection(f.ctx, c.ConnectionID))
+	f.assertConnectionDeleted(t, c)
 }
