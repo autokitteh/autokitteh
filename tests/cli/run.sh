70ea88e03253c19aec16f8a0cd1d15c0736db566
Igor Rayak <igor@autokitteh.com>
2024-03-17 16:00:01 +0200

    fix CLI tests

--- tests/cli/run.sh
+++ tests/cli/run.sh
@@ -67,9 +67,9 @@ trap ontrap 0
 export AK
 
 for f in tests/cli/${TESTS}; do
-    up "$(basename "${f}")"
+    up "${f##*/}"
 
-    AK="${PWD}/${ak_filename} --url http://$(cat ${addr_filename})"
+    AK="${PWD}/${ak_filename} -c http.service_url=http://$(< ${addr_filename})"
 
     echo "--- ${f} ---"
     ./scripts/clitest -1 "${f}"
