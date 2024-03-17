81fa9e75b0f9f19835f324621bf3ea8075b7a17c
Miki Tebeka <miki@autokitteh.com>
2024-03-17 11:07:40 +0200

    shellcheck fixes

--- runtimes/pythonrt/testdata/create-workflow.sh
+++ runtimes/pythonrt/testdata/create-workflow.sh
@@ -5,7 +5,7 @@ set -e
 
 ak manifest apply testdata/simple/autokitteh.yaml
 tmp=$(mktemp)
-ak project build py_simple --from testdata/simple/ | tee ${tmp}
-build_id=$(cat ${tmp} | awk '{print $2}')
+ak project build py_simple --from testdata/simple/ | tee "${tmp}"
+build_id=$(awk '{print $2}' "${tmp}")
 env_id=$(ak envs list | grep default | awk -F\" '{print $2}')
-ak deployment create --build-id ${build_id} --activate --env ${env_id}
+ak deployment create --build-id "${build_id}" --activate --env "${env_id}"
