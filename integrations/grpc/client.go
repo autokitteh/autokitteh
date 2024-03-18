7a18d9d4d7e67a740c21dc810b4bd8630ac7dbe0
Efi Shtain <efi@autokitteh.com>
2024-03-18 09:52:00 +0200

    pr updates

--- integrations/grpc/client.go
+++ integrations/grpc/client.go
@@ -4,16 +4,12 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 
 	"github.com/fullstorydev/grpcurl"
-	"github.com/iancoleman/strcase"
-	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/grpcreflect"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/protobuf/types/descriptorpb"
 )
 
 type method struct {
@@ -42,87 +38,6 @@ func newGRPCClient(conn *grpc.ClientConn) (*grpcclient, error) {
 		reflectionClient: reflectionClient,
 		descSource:       descSource,
 	}, nil
-}
-
-// proto.names should be a list of proto files
-// proto.paths should be a list of paths where proto files could be found
-// both has to be provided, otherwise an error is returned
-// for example:
-// proto.names = []string{"mysvc.proto", "mysvc2.proto"}
-// proto.paths = []string{"/actual/path/to/protos/basedir"}
-//
-//lint:ignore U1000 Ignore unused to keep this code until we decide if we need it
-func newGRPCClientWithProtos(conn *grpc.ClientConn, protos protoFiles) (*grpcclient, error) {
-	if len(protos.names) == 0 || len(protos.paths) == 0 {
-		return nil, errors.New("protos has to have both names and paths")
-	}
-
-	c, err := newGRPCClient(conn)
-	if err != nil {
-		return nil, err
-	}
-
-	fileDesc, err := grpcurl.DescriptorSourceFromProtoFiles(protos.paths, protos.names...)
-	if err != nil {
-		return nil, err
-	}
-
-	c.descSource = compositeSource{
-		reflection: c.descSource,
-		file:       fileDesc,
-	}
-
-	return c, nil
-}
-
-//lint:ignore U1000 Ignore unused to keep this code until we decide if we need it
-func (r *grpcclient) getServiceDescriptor(service string) (*desc.ServiceDescriptor, error) {
-	d, err := r.descSource.FindSymbol(service)
-	if err != nil {
-		return nil, err
-	}
-
-	svc, ok := d.(*desc.ServiceDescriptor)
-	if !ok {
-		return nil, errors.New("supplied desc is not a service descriptor")
-	}
-
-	return svc, nil
-}
-
-//lint:ignore U1000 Ignore unused to keep this code until we decide if we need it
-func (r *grpcclient) listMethods(service string) ([]method, error) {
-	svc, err := r.getServiceDescriptor(service)
-	if err != nil {
-		return nil, err
-	}
-
-	var methods []method
-
-	for _, m := range svc.GetMethods() {
-		var (
-			inputs, constants []string
-		)
-
-		for _, fd := range m.GetInputType().GetFields() {
-			if fd.GetType() == descriptorpb.FieldDescriptorProto_TYPE_ENUM {
-				for _, v := range fd.GetEnumType().GetValues() {
-					constants = append(constants, v.GetName())
-				}
-			}
-			inputs = append(inputs, fd.GetJSONName())
-		}
-
-		name := strcase.ToSnake(fmt.Sprintf("%s%s", svc.GetName(), m.GetName()))
-		methods = append(methods, method{
-			Name:      name,
-			Fullname:  m.GetFullyQualifiedName(),
-			Inputs:    inputs,
-			Constants: constants,
-		})
-	}
-
-	return methods, nil
 }
 
 func (r *grpcclient) invoke(method string, payload map[string]any) (map[string]interface{}, error) {
