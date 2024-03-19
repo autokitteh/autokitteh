fc6f0daf5054926f3d70c4f9af43630c986e7362
efi shtain <efi@autokitteh.com>
2024-03-19 17:06:22 +0200

    GRPC Integration minimal implementation (#97)
    this is a simplified implementation for grpc integration (with example)
    support only actions to grpc and not listening for triggers

--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/bufbuild/protovalidate-go v0.6.0
 	github.com/fatih/color v1.16.0
 	github.com/flosch/pongo2/v6 v6.0.0
+	github.com/fullstorydev/grpcurl v1.8.9
 	github.com/glebarez/sqlite v1.10.0
 	github.com/google/go-cmp v0.6.0
 	github.com/google/go-github/v60 v60.0.0
@@ -34,6 +35,7 @@ require (
 	github.com/hexops/gotextdiff v1.0.3
 	github.com/iancoleman/strcase v0.3.0
 	github.com/invopop/jsonschema v0.12.0
+	github.com/jhump/protoreflect v1.15.6
 	github.com/joho/godotenv v1.5.1
 	github.com/josephburnett/jd v1.8.1
 	github.com/knadh/koanf/parsers/yaml v0.1.0
@@ -67,6 +69,7 @@ require (
 	golang.org/x/text v0.14.0
 	golang.org/x/tools v0.19.0
 	google.golang.org/api v0.169.0
+	google.golang.org/grpc v1.62.1
 	google.golang.org/protobuf v1.33.0
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -103,6 +106,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.4 // indirect
 	github.com/aws/smithy-go v1.20.1 // indirect
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/bufbuild/protocompile v0.8.0 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -198,7 +202,6 @@ require (
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240311173647-c811ad7063a7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240311173647-c811ad7063a7 // indirect
-	google.golang.org/grpc v1.62.1 // indirect
 	gorm.io/driver/mysql v1.5.4 // indirect
 	modernc.org/libc v1.44.1 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
--- integrations/grpc/module.go
+++ integrations/grpc/module.go
@@ -1,9 +1,20 @@
 package grpc
 
-/*
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"go.autokitteh.dev/autokitteh/internal/kittehs"
+	"go.autokitteh.dev/autokitteh/sdk/sdkexecutor"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 var safeForJsonWrapper = sdktypes.ValueWrapper{SafeForJSON: true}
 
-func parsePayload(args []sdktypes.Value, kwargs map[string]sdktypes.Value) (map[string]any, error) {
+func parseArgs(args []sdktypes.Value, kwargs map[string]sdktypes.Value) (map[string]any, error) {
 	if len(args) > 1 {
 		return nil, errors.New("args len should be 0 or 1")
 	}
@@ -13,7 +24,7 @@ func parsePayload(args []sdktypes.Value, kwargs map[string]sdktypes.Value) (map[
 			return nil, errors.New("either provide one dict arg or kwargs")
 		}
 
-		if !sdktypes.IsDictValue(args[0]) {
+		if !args[0].IsDict() {
 			return nil, errors.New("args has to be dict")
 		}
 
@@ -34,81 +45,51 @@ func parsePayload(args []sdktypes.Value, kwargs map[string]sdktypes.Value) (map[
 	})
 }
 
-func createGRPCCallWrapper(functionName string) sdkexecutor.Function {
+func handleGenericGRPCCall() sdkexecutor.Function {
 	return func(ctx context.Context, v []sdktypes.Value, m map[string]sdktypes.Value) (sdktypes.Value, error) {
-		payload, err := parsePayload(v, m)
+		args, err := parseArgs(v, m)
 		if err != nil {
-			return nil, err
+			return sdktypes.Nothing, err
 		}
 
-		hostport := string(sdkmodule.FunctionDataFromContext(ctx))
+		hostport, ok := args["host"].(string)
+		if !ok {
+			return sdktypes.Nothing, errors.New("host is required")
+		}
 
 		conn, err := grpc.Dial(hostport, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
-			return nil, err
+			return sdktypes.Nothing, err
 		}
 		defer conn.Close()
 
 		s, err := newGRPCClient(conn)
 		if err != nil {
-			return nil, err
+			return sdktypes.Nothing, err
 		}
 
-		res, err := s.invoke(functionName, payload)
+		service, ok := args["service"].(string)
+		if !ok {
+			return sdktypes.Nothing, errors.New("service is required")
+		}
+		method, ok := args["method"].(string)
+		if !ok {
+			return sdktypes.Nothing, errors.New("method is required")
+		}
+
+		payload := map[string]any{}
+		if data, ok := args["payload"]; ok {
+			if payload, ok = data.(map[string]any); !ok {
+				return sdktypes.Nothing, errors.New("payload has to be dict")
+			}
+		}
+
+		funcName := fmt.Sprintf("%s.%s", service, method)
+		res, err := s.invoke(funcName, payload)
 		if err != nil {
-			return nil, err
+			return sdktypes.Nothing, err
 		}
 
 		return sdktypes.DefaultValueWrapper.Wrap(res)
 	}
 }
-
-func newGRPCModule(xid sdktypes.ExecutorID) sdkmodule.Module {
-	addr := string(config)
-
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-
-	s, err := newGRPCClient(conn)
-	if err != nil {
-		return nil, err
-	}
-
-	svcs, err := s.descSource.ListServices()
-	if err != nil {
-		return nil, err
-	}
-
-	var fns []method
-	for _, svc := range svcs {
-		methods, err := s.listMethods(svc)
-		if err != nil {
-			return nil, err
-		}
-		fns = append(fns, methods...)
-	}
-
-	sort.SliceStable(fns, func(i, j int) bool {
-		return fns[i].Name < fns[j].Name
-	})
-
-	opts := []sdkmodule.Optfn{
-		sdkmodule.WithConfigAsData(),
-	}
-
-	for _, f := range fns {
-		opts = append(opts, kittehs.Transform(f.Constants, func(c string) sdkmodule.Optfn {
-			return sdkmodule.ExportValue(c, sdkmodule.WithValue(kittehs.Must1(sdktypes.WrapValue(c))))
-		})...)
-	}
-
-	opts = append(opts, kittehs.Transform(fns, func(f method) sdkmodule.Optfn {
-		return sdkmodule.ExportFunction(f.Name, createGRPCCallWrapper(f.Fullname), sdkmodule.WithArgs(f.Inputs...))
-	})...)
-
-	return sdkmodule.New(opts...)
-}
-*/
