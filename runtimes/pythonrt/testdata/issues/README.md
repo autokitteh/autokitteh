45cfde4ae231caf962f1a7134de1870c58ccf559
Miki Tebeka <miki@autokitteh.com>
2024-03-20 18:01:38 +0200

    docs & post to slack

--- runtimes/pythonrt/testdata/issues/README.md
+++ runtimes/pythonrt/testdata/issues/README.md
@@ -3,3 +3,32 @@
 **WARNING: WIP**
 
 When GitHub issue is opened, notify on slack.
+
+## Running
+
+Run temporal:
+
+```
+$ temporal server start-dev
+```
+
+Run autokitteh:
+
+```
+$ ak up -m dev
+```
+
+Create an ngrok tunnel:
+
+```
+$ ngrok http --domain autokitteh-miki.ngrok.dev 9980
+```
+
+- Deploy the manifset (see ../deploy.go)
+- [Create a new issue][issue]
+- A new message should appear in `#slack-test` channel on autokitteh Slack
+
+
+
+[ghs]: https://github.com/organizations/testkitteh/settings/apps/autokitteh-miki
+[new]: https://github.com/testkitteh/miki/issues/
--- runtimes/pythonrt/testdata/issues/issues.py
+++ runtimes/pythonrt/testdata/issues/issues.py
@@ -14,33 +14,33 @@ logging.basicConfig(
 )
 
 def on_issue(event):
+    event = event['data']
     if event['action'] != 'opened':
         logging.info('skipping event of type %r', event['action'])
         return
 
     issue = event['issue']
     title, number, login, url = \
-        issue['title'], issue['number'], issue['user']['login'], issue['url']
+        issue['title'], issue['number'], issue['user']['login'], issue['html_url']
 
     logging.info('issue %s: %s by %s', number, title, login)
-    text = {
-        'type': 'mrkdwn',
-        'text': f'<{url}|Issue #{number}>: {title} opened by {login}',
-    }
+    text = f'Issue #{number}: {title} opened by {login}, see {url}'
 
     client = WebClient(token=SLACK_TOKEN)
-    client.chat_post_message(channel=CHANNEL_ID, text=text)
+    client.chat_postMessage(channel=CHANNEL_ID, text=text)
 
 
 if __name__ == '__main__':
     event = {
-        'action': 'opened',
-        'issue': {
-            'title': 'Fix url',
-            'number': 1,
-            'user': {'login': 'tebeka'},
-            'url': 'https://api.github.com/repos/tebeka/toggl/issues/1',
-        }
+        'data': {
+            'action': 'opened',
+            'issue': {
+                'title': 'Fix url',
+                'number': 1,
+                'user': {'login': 'tebeka'},
+                'html_url': 'https://api.github.com/repos/tebeka/toggl/issues/1',
+            },
+        },
     }
 
     on_issue(event)
