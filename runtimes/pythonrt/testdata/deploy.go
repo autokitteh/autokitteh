38b92d0af7a0068a15dab7b53d6df9d0a7d0e123
Miki Tebeka <miki@autokitteh.com>
2024-03-20 18:00:25 +0200

    json

--- runtimes/pythonrt/testdata/deploy.go
+++ runtimes/pythonrt/testdata/deploy.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path"
-	"strings"
 
 	"gopkg.in/yaml.v3"
 )
+
+func defaultEnvID() (string, error) {
+	dec := json.NewDecoder(&buf)
+	var env struct {
+		ID   string `json:"env_id"`
+		Name string
+	}
+
+	for {
+		err := dec.Decode(&env)
+		if errors.Is(err, io.EOF) {
+			return "", fmt.Errorf("can't find default env in env list")
+		}
+
+		if err != nil {
+			return "", fmt.Errorf("env list - %w", err)
+		}
+
+		if env.Name == "default" {
+			return env.ID, nil
+		}
+	}
+}
 
 func main() {
 	flag.Usage = func() {
@@ -44,6 +68,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	envID, err := defaultEnvID()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: can't find default env - %s\n", err)
+		os.Exit(1)
+	}
+
 	cmd := exec.Command("ak", "manifest", "apply", mfstFile)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -53,7 +83,7 @@ func main() {
 	}
 
 	var buf bytes.Buffer
-	cmd = exec.Command("ak", "project", "build", mfst.Project.Name, "--from", dir)
+	cmd = exec.Command("ak", "-j", "project", "build", mfst.Project.Name, "--from", dir)
 	cmd.Stdout = &buf
 	cmd.Stderr = os.Stderr
 
@@ -62,55 +92,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	// build_id: bld_60mfa7qs3927q8j7kc4g4bancx
-	out := buf.String()
-	i := strings.Index(out, " ")
-	if i == -1 {
-		fmt.Fprintf(os.Stderr, "error: bad build output - %q\n", out)
+	var build struct {
+		ID string `json:"build_id"`
+	}
+
+	if err := json.NewDecoder(&buf).Decode(&build); err != nil {
+		fmt.Fprintf(os.Stderr, "error: bad build output - %q\n", buf.String())
 		os.Exit(1)
 	}
-	buildID := buf.String()[i+1 : len(out)-1] // ignore \n
+	fmt.Println("build ID:", build.ID)
 
-	buf.Reset()
-
-	cmd = exec.Command("ak", "env", "list")
-	cmd.Stdout = &buf
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: can't get env list - %s\n", err)
-		os.Exit(1)
-	}
-
-	// env_id:"env_60j2xb5s3927q8j7kc4g4bancx"  project_id:"prj_60h3snbs3927q8j7kc4g4bancx"  name:"default"
-	envID := ""
-	s := bufio.NewScanner(&buf)
-	for s.Scan() {
-		if strings.Contains(s.Text(), `name:"default"`) {
-			i := strings.Index(s.Text(), `"`)
-			if i == -1 {
-				fmt.Fprintf(os.Stderr, "error: bad env line - %q\n", s.Text())
-				os.Exit(1)
-			}
-
-			j := strings.Index(s.Text()[i+1:], `"`)
-			if j == -1 {
-				fmt.Fprintf(os.Stderr, "error: bad env line - %q\n", s.Text())
-				os.Exit(1)
-			}
-			envID = s.Text()[i+1 : i+j+1]
-		}
-	}
-	if err := s.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: can't scan env list - %s\n", err)
-		os.Exit(1)
-	}
-
-	if envID == "" {
-		fmt.Fprintln(os.Stderr, "error: can't find default env ID")
-		os.Exit(1)
-	}
-
-	cmd = exec.Command("ak", "deployment", "create", "--build-id", buildID, "--env", envID, "--activate")
+	cmd = exec.Command("ak", "deployment", "create", "--build-id", build.ID, "--env", envID, "--activate")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
