e5d9920a16d5269690cbfe1f0971b61fd3ae8772
Efi Shtain <efi@autokitteh.com>
2024-03-31 15:49:14 +0300

    .

---
+++ integrations/github/workflows.go
@@ -0,0 +1,41 @@
+package github
+
+import (
+	"context"
+
+	"github.com/google/go-github/v60/github"
+
+	"go.autokitteh.dev/autokitteh/sdk/sdkmodule"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+)
+
+// https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
+func (i integration) triggerWorkflow(ctx context.Context, args []sdktypes.Value, kwargs map[string]sdktypes.Value) (sdktypes.Value, error) {
+	var (
+		owner, repo, workflowName string
+		req                       github.CreateWorkflowDispatchEventRequest
+	)
+
+	if err := sdkmodule.UnpackArgs(args, kwargs,
+		"owner", &owner,
+		"repo", &repo,
+		"ref", &req.Ref,
+		"workflow_name", &workflowName,
+		"inputs?", &req.Inputs,
+	); err != nil {
+		return sdktypes.InvalidValue, err
+	}
+
+	// Invoke the API method.
+	gh, err := i.NewClient(ctx)
+	if err != nil {
+		return sdktypes.InvalidValue, err
+	}
+
+	resp, err := gh.Actions.CreateWorkflowDispatchEventByFileName(ctx, owner, repo, workflowName, req)
+	if err != nil {
+		return sdktypes.InvalidValue, err
+	}
+
+	return sdktypes.WrapValue(resp)
+}
