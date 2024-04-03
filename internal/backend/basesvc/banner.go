3d4fe6d14a2d290bd78eb8a4d08a9f420e0aaca2
Itay Donanhirsh <itay@autokitteh.com>
2024-04-03 00:10:41 -0700

    ENG-582: start temporal ui; add easy redirection to it (#142)
    - when starting temporal dev server, enable ui
    - print temporal and autokitteh UI addresses in clickable format in
    terminal
    - print ipv4 address, if possible, for autokitteh server
    
    
    ![image](https://github.com/autokitteh/autokitteh/assets/426603/e6041f60-1198-44e8-9c6f-bab339354d6d)

---
+++ internal/backend/basesvc/banner.go
@@ -0,0 +1,55 @@
+package basesvc
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"text/template"
+
+	"github.com/fatih/color"
+
+	"go.autokitteh.dev/autokitteh/internal/kittehs"
+	"go.autokitteh.dev/autokitteh/internal/version"
+)
+
+//go:embed banner.txt
+var banner string
+
+var bannerTemplate = template.Must(template.New("banner").Parse(banner))
+
+func printBanner(opts RunOptions, addr, temporalFrontendAddr, temporalUIAddr string) {
+	fieldColor := color.New(color.FgBlue).Add(color.Bold).SprintFunc()
+	eyeColor := color.New(color.FgGreen).Add(color.Bold).SprintFunc()
+
+	var mode string
+	if opts.Mode != "" {
+		mode = "Mode:        " + fieldColor(opts.Mode) + " "
+	}
+
+	if temporalFrontendAddr != "" {
+		temporalFrontendAddr = "Temporal:    " + fieldColor(temporalFrontendAddr) + " "
+	}
+
+	if temporalUIAddr != "" {
+		temporalUIAddr = "Temporal UI: " + fieldColor(temporalUIAddr) + " "
+	}
+
+	kittehs.Must0(bannerTemplate.Execute(os.Stderr, struct {
+		Version              string
+		PID                  string
+		Addr                 string
+		Eye                  string
+		UIAddr               string
+		Mode                 string
+		Temporal, TemporalUI string
+	}{
+		Version:    fieldColor(version.Version),
+		PID:        fieldColor(fmt.Sprintf("%d", os.Getpid())),
+		Addr:       fieldColor(addr),
+		UIAddr:     fieldColor(fmt.Sprintf("http://%s", addr)),
+		Eye:        eyeColor("▀"),
+		Mode:       mode,
+		Temporal:   temporalFrontendAddr,
+		TemporalUI: temporalUIAddr,
+	}))
+}
