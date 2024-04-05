2d445418812766125b35f3069690ca3fef9768ca
Daniel Abraham <daniel@autokitteh.com>
2024-04-05 17:48:00 +0300

    Fix gRPC logo

--- integrations/grpc/svc.go
+++ integrations/grpc/svc.go
@@ -14,7 +14,7 @@ var description = kittehs.Must1(sdktypes.StrictIntegrationFromProto(&sdktypes.In
 	IntegrationId: integrationID.String(),
 	UniqueName:    "grpc",
 	DisplayName:   "GRPC",
-	LogoUrl:       "/static/images/http.svg",
+	LogoUrl:       "/static/images/grpc.png",
 }))
 
 func New(sec sdkservices.Secrets) sdkservices.Integration {
