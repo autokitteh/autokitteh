08227f72e6780c48535a40752bba4991a73e7909
Daniel Abraham <daniel@autokitteh.com>
2024-04-11 16:13:46 +0300

    Tweaks in server configs

--- tests/system/server.go
+++ tests/system/server.go
@@ -17,7 +17,8 @@ var useProcSvc, _ = strconv.ParseBool(os.Getenv("AK_SYSTEST_USE_PROC_SVC"))
 // subprocess: to support breakpoint debugging, and measure test coverage.
 func startAKServer(ctx context.Context, akPath string) (svc.Service, error) {
 	cfg := kittehs.Must1(svc.LoadConfig("", map[string]any{
-		"db.dsn":    "sqlite:test.sqlite",
+		"db.type":   "sqlite",
+		"db.dsn":    "sqlite:autokitteh.sqlite",
 		"http.addr": ":0",
 	}, ""))
 
