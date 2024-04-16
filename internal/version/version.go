0f9e62803416a237285ecc94cc4476b98022ce5f
Itay Donanhirsh <itay@autokitteh.com>
2024-04-16 13:38:10 -0700

    don't print all version fields on cmd if unset (#192)

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,10 +1,4 @@
 package version
 
 // Supplied by linker from the Makefile.
-var (
-	Version = "unset"
-	Commit  = "unset"
-	Time    = "unset"
-	User    = "unset"
-	Host    = "unset"
-)
+var Version, Commit, Time, User, Host string
