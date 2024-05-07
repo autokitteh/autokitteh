1af8cad26782595c9ba41d18b3f9e7a019980ddf
Itay Donanhirsh <itay@autokitteh.com>
2024-05-06 23:49:41 -0700

    easy token generation (#248)
    new link `/token` where just a token is displayed.
    
    
    ![image](https://github.com/autokitteh/autokitteh/assets/426603/78c09874-7d7f-47b2-b6e7-dc79d47d9562)

---
+++ internal/backend/dashboardsvc/token.go
@@ -0,0 +1,30 @@
+package dashboardsvc
+
+import (
+	"net/http"
+
+	"go.autokitteh.dev/autokitteh/web/webdashboard"
+)
+
+func (s Svc) initToken() {
+	s.Muxes.Auth.HandleFunc("GET /token", s.token)
+}
+
+func (s Svc) token(w http.ResponseWriter, r *http.Request) {
+	tok, err := s.Svcs.Auth().CreateToken(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := webdashboard.Tmpl(r).ExecuteTemplate(w, "token.html", struct {
+		Message string
+		Title   string
+		Token   string
+	}{
+		Title: "Generate Token",
+		Token: tok,
+	}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
