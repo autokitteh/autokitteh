ea0b5de31e6929d36606fa3ea88e9e9ea082b097
Daniel Abraham <daniel@autokitteh.com>
2024-05-08 16:30:33 +0300

    Undelete the actual opening of a web-socket (#258)

--- integrations/slack/internal/vars/vars.go
+++ integrations/slack/internal/vars/vars.go
@@ -10,15 +10,15 @@ type Vars struct {
 	AppID        string
 	EnterpriseID string
 	TeamID       string
-
-	AppToken string `var:"secret"`
 }
 
 var (
-	BotTokenName  = sdktypes.NewSymbol("bot_token")
-	KeyName       = sdktypes.NewSymbol("key")
-	WebSocketName = sdktypes.NewSymbol("websocket")
-	OAuthDataName = sdktypes.NewSymbol("oauth_data")
+	// Socket Mode
+	AppTokenName = sdktypes.NewSymbol("AppToken")
+	BotTokenName = sdktypes.NewSymbol("BotToken")
+
+	KeyName       = sdktypes.NewSymbol("Key")
+	OAuthDataName = sdktypes.NewSymbol("OAuthData")
 )
 
 func KeyValue(appID, enterpriseID, teamID string) string {
