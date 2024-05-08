1ca1ea36aac3ae97a00c3971414d699f2884c4fd
Daniel Abraham <daniel@autokitteh.com>
2024-05-08 16:09:56 +0300

    Finishing touches

--- integrations/slack/websockets/bot_events.go
+++ integrations/slack/websockets/bot_events.go
@@ -85,7 +85,7 @@ func (h handler) handleBotEvent(e *socketmode.Event, c *socketmode.Client) {
 	}
 
 	// Retrieve all the relevant connections for this event.
-	cids, err := h.vars.FindConnectionIDs(context.Background(), h.integrationID, vars.BotTokenName, "")
+	cids, err := h.vars.FindConnectionIDs(context.Background(), h.integrationID, vars.AppTokenName, "")
 	if err != nil {
 		h.logger.Error("Failed to retrieve connection tokens", zap.Error(err))
 		return
