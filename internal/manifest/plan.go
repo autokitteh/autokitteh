0634d25c79ad2d5335d2ca4b344b1c970559f80f
Igor Rayak <igor@autokitteh.com>
2024-05-13 00:38:53 +0300

    fix trigger validation on manifest plan / tests

--- internal/manifest/plan.go
+++ internal/manifest/plan.go
@@ -427,7 +427,7 @@ func planTriggers(ctx context.Context, mtriggers []*Trigger, client sdkservices.
 			return nil, fmt.Errorf("trigger %q: invalid entrypoint: %w", mtrigger.GetKey(), err)
 		}
 
-		// keep schedule in data section
+		// schedule could be specified in manifest either in dedicated or in `data' sections. Ensure schedule is present in data section
 		if mtrigger.Schedule != "" {
 			if schedule, found := mtrigger.Data["schedule"]; found {
 				if schedule != mtrigger.Schedule {
