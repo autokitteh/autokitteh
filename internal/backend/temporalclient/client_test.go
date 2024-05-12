f4ade09e4d16578c1371832958632d9d69872ef0
Miki Tebeka <miki@autokitteh.com>
2024-05-12 10:45:51 +0300

    [ENG-544] Make temporal dev server shut up
    Currently using a replace directive to get the temporal sdk with out
    patch commited. Once they cut a new version, we'll switch (ENG-820)

---
+++ internal/backend/temporalclient/client_test.go
@@ -0,0 +1,44 @@
+package temporalclient
+
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/testsuite"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestStartDevServer(t *testing.T) {
+	ctx := context.Background()
+	logger := zaptest.NewLogger(t)
+	cfg := &Config{
+		DevServer: testsuite.DevServerOptions{},
+	}
+	opts := client.Options{}
+
+	c := &impl{
+		done: make(chan struct{}),
+		z:    logger,
+	}
+	err := c.startDevServer(ctx, cfg, opts)
+	require.NoError(t, err)
+	defer c.Stop(ctx)
+
+	require.NotNil(t, c.client)
+	require.NotNil(t, c.srv)
+	require.NotNil(t, c.srvLog)
+
+	time.Sleep(100 * time.Millisecond) // Let the server emit some logs
+
+	file, err := os.Open(c.srvLog.Name())
+	require.NoError(t, err)
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	require.NoError(t, err)
+	require.NotEmpty(t, data)
+}
