be72f4055b41e45e06577b82f03f1fb87e0e5497
Itay Donanhirsh <itay@autokitteh.com>
2024-05-14 21:14:46 -0700

    checks

--- sdk/sdkmodule/unpack.go
+++ sdk/sdkmodule/unpack.go
@@ -3,6 +3,7 @@ package sdkmodule
 import (
 	"fmt"
 	"maps"
+	"reflect"
 	"strings"
 
 	"go.autokitteh.dev/autokitteh/internal/kittehs"
@@ -16,6 +17,11 @@ import (
 // If the parameter name ends with "=", it must be supplied in kwargs.
 // If the parameter name starts with "**", the destination will accept all kwargs as a dict.
 // If the parameter name starts with "*", the destination will aceppt all args as a list.
+//
+// A nameless parameter can also be specified. That parameter must be a pointer to a struct.
+// The function will use the member names of the struct as the parameter names. If the fields
+// are tagged with `json:"..."`, the tag will be used as the parameter name. If the tag is
+// "-", the field will be ignored. If the tag modifier is "omitempty", the field will be optional.
 //
 // Example:
 //
@@ -37,14 +43,64 @@ import (
 // it essentially does the same thing, but on autokitteh level and not starlark)
 //
 // TODO: varargs.
-func UnpackArgs(args []sdktypes.Value, kwargs map[string]sdktypes.Value, dsts ...interface{}) error {
+func UnpackArgs(args []sdktypes.Value, kwargs map[string]sdktypes.Value, dsts ...any) error {
 	kwargs = maps.Clone(kwargs)
 
-	if len(dsts)%2 != 0 {
-		return fmt.Errorf("must have even number of dsts")
+	var flattened []any
+
+	for i := 0; i < len(dsts); i++ {
+		if _, ok := dsts[i].(string); ok {
+			i++
+			continue
+		}
+
+		t := reflect.TypeOf(dsts[i])
+		if t.Kind() != reflect.Ptr {
+			return fmt.Errorf("dst %d must be name or a pointer to a struct", i)
+		}
+
+		tt := t.Elem()
+		if tt.Kind() != reflect.Struct {
+			return fmt.Errorf("dst %d must be a pointer to a struct", i)
+		}
+
+		for j := 0; j < tt.NumField(); j++ {
+			ttf := tt.Field(j)
+
+			name := ttf.Name
+			optional := ttf.Type.Kind() == reflect.Ptr
+
+			if j := ttf.Tag.Get("json"); j != "" {
+				if j == "-" {
+					continue
+				}
+
+				var rest string
+				name, rest, _ = strings.Cut(j, ",")
+				if rest == "omitempty" {
+					optional = true
+				}
+			}
+
+			if len(name) == 0 {
+				continue
+			}
+
+			name += "="
+
+			if optional {
+				name += "?"
+			}
+
+			flattened = append(flattened, name, reflect.ValueOf(dsts[i]).Elem().Field(j).Addr().Interface())
+		}
+
+		dsts = append(dsts[:i], dsts[i+1:]...)
 	}
 
-	for i := 0; i < len(dsts); i += 2 {
+	dsts = append(dsts, flattened...)
+
+	for i := 0; i+1 < len(dsts); i += 2 {
 		nameitf, dst := dsts[i], dsts[i+1]
 
 		name, ok := nameitf.(string)
--- sdk/sdkmodule/unpack_test.go
+++ sdk/sdkmodule/unpack_test.go
@@ -12,11 +12,18 @@ import (
 func TestUnpack(t *testing.T) {
 	assert.NoError(t, UnpackArgs(nil, nil))
 
-	var i int
-	assert.Error(t, UnpackArgs(nil, nil, "i", &i))
+	var (
+		i  int
+		st struct {
+			X int
+		}
+	)
+
+	assert.Error(t, UnpackArgs(nil, nil, "i", &i, &st))
 
 	if assert.NoError(t, UnpackArgs([]sdktypes.Value{sdktypes.NewIntegerValue(42)}, nil, "i", &i)) {
 		assert.Equal(t, 42, i)
+		assert.Zero(t, st)
 	}
 
 	var s string
@@ -26,5 +33,38 @@ func TestUnpack(t *testing.T) {
 	}, "i", &i, "s=", &s)) {
 		assert.Equal(t, 64, i)
 		assert.Equal(t, "meow", s)
+		assert.Zero(t, st)
 	}
+}
+
+func TestUnpackFlat(t *testing.T) {
+	var (
+		i  int
+		st struct {
+			X int     `json:"x"`
+			Y *string `json:"y"`
+			S struct {
+				Z int `json:"z"`
+			} `json:"s"`
+			Sptr *struct {
+				Z int `json:"z"`
+			} `json:"s"`
+		}
+	)
+
+	assert.NoError(t, UnpackArgs([]sdktypes.Value{
+		sdktypes.NewIntegerValue(42),
+	}, map[string]sdktypes.Value{
+		"x": sdktypes.NewIntegerValue(64),
+		"y": sdktypes.NewStringValue("meow"),
+		"s": sdktypes.NewDictValueFromStringMap(map[string]sdktypes.Value{
+			"z": sdktypes.NewIntegerValue(128),
+		}),
+	}, "i", &i, &st))
+
+	assert.Equal(t, 42, i)
+	assert.Equal(t, 64, st.X)
+	assert.Equal(t, "meow", *st.Y)
+	assert.Equal(t, 128, st.S.Z)
+	assert.Zero(t, st.Sptr)
 }
