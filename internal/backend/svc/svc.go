5059f2cb4e034b9277160bd95987dbe4df707b00
Ran Magen <enricomagen@gmail.com>
2024-05-15 14:22:37 -0700

    Fix typo in authhttmiddleware

--- internal/backend/svc/svc.go
+++ internal/backend/svc/svc.go
@@ -126,7 +126,7 @@ func makeFxOpts(cfg *Config, opts RunOptions) []fx.Option {
 		Component("auth", configset.Empty, fx.Provide(authsvc.New)),
 		Component("authjwttokens", authjwttokens.Configs, fx.Provide(authjwttokens.New)),
 		Component("authsessions", authsessions.Configs, fx.Provide(authsessions.New)),
-		Component("authhttmiddleware", authhttpmiddleware.Configs, fx.Provide(authhttpmiddleware.New)),
+		Component("authhttpmiddleware", authhttpmiddleware.Configs, fx.Provide(authhttpmiddleware.New)),
 
 		DBFxOpt(),
 		Component(
