051aaf1f87fbc74d336b652e66ca3fdd6c9cf879
Miki Tebeka <miki@autokitteh.com>
2024-05-16 07:28:44 +0300

    Bypass import hook so user can call their module in any name

--- runtimes/pythonrt/ak_runner.py
+++ runtimes/pythonrt/ak_runner.py
@@ -11,12 +11,10 @@ import pickle
 import sys
 import tarfile
 from base64 import b64decode, b64encode
-from functools import wraps
-from importlib.abc import Loader
-from importlib.machinery import SourceFileLoader
 from os import mkdir
 from pathlib import Path
 from socket import AF_UNIX, SOCK_STREAM, socket
+from types import ModuleType
 
 # Use own own logger, leave root logger to user.
 log = logging.getLogger('AK')
@@ -69,82 +67,25 @@ class Transformer(ast.NodeTransformer):
         return call
 
 
-class AKLoader(Loader):
-    """Custom file loaders that will rewrite function calls to actions."""
-    def __init__(self, src_loader, action):
-        self.file_name = src_loader.path
-        self.module_name = src_loader.name
-        self.action = action
-
-    def create_module(self, spec):
-        # Must be defined since it's an abstract method
-        return None  # Use default module
-
-    def exec_module(self, module):
-        try:
-            with open(self.file_name) as fp:
-                src = fp.read()
-        except OSError as err:
-            raise ImportError(f'cannot read {self.module_name!r} - {err}')
-
-        mod = ast.parse(src, self.file_name, 'exec')
-        trans = Transformer(self.file_name)
-        out = trans.visit(mod)
-        ast.fix_missing_locations(out)
-
-        code = compile(out, self.file_name, 'exec')
-        setattr(module, ACTION_NAME, self.action)
-        exec(code, module.__dict__)
-
-
-# There is an established way to add import hooks, but we want to change the behavior of
-# the current PathFinder found in sys.import_hooks so it'll call our code when importing
-# form the user directory. This is why you'll see all these monkey patches below.
-
-def patch_finder(finder, action):
-    """Patches the finder to use a custom source loader."""
-    _orig_find_spec = finder.find_spec
-    def find_spec(fullname, target=None):
-        spec = _orig_find_spec(fullname, target)
-        if spec is None or not isinstance(spec.loader, SourceFileLoader):
-            return spec
-
-        log.info('patching loader for %r', fullname)
-        spec.loader = AKLoader(spec.loader, action)
-        return spec
-
-    finder.find_spec = find_spec
-
-
-def wrap_hook(hook, user_dir, action):
-    """Wraps a hook to patch finder on user code directories."""
-    @wraps(hook)
-    def wrapper(path):
-        finder = hook(path)
-        if user_dir.is_relative_to(path):
-            patch_finder(finder, action)
-        return finder
-
-    return wrapper
-
-
-def patch_import_hooks(user_dir, action_fn):
-    """Patches standard import hook in sys.path_hooks."""
-    user_dir = Path(user_dir)
-    for i, hook in enumerate(sys.path_hooks):
-        if hook.__name__ == 'path_hook_for_FileFinder':
-            sys.path_hooks[i] = wrap_hook(hook, user_dir, action_fn)
-            return
-
-    raise RuntimeError(f'cannot find import hook to patch in {sys.path_hooks}')
-
-
 def load_code(root_path, action_fn, module_name):
-    patch_import_hooks(root_path, action_fn)
-    sys.path.insert(0, str(root_path))
+    """Load user code into a module, instrumenting function calls."""
     log.info('importing %r', module_name)
-    mod = __import__(module_name)
-    return mod
+    file_name = Path(root_path) / (module_name + '.py')
+    with open(file_name) as fp:
+        src = fp.read()
+
+    tree = ast.parse(src, file_name, 'exec')
+    trans = Transformer(file_name)
+    patched_tree = trans.visit(tree)
+    ast.fix_missing_locations(patched_tree)
+
+    code = compile(patched_tree, file_name, 'exec')
+
+    module = ModuleType(module_name)
+    setattr(module, ACTION_NAME, action_fn)
+    exec(code, module.__dict__)
+
+    return module
 
 
 def run_code(mod, entry_point, data):
