aa2b0c0c65747875b62a03c7d0f06ca0eeb32671
Daniel Abraham <daniel@autokitteh.com>
2024-05-17 10:12:25 -0700

    Add "search users" to GitHub integration

--- integrations/github/users.go
+++ integrations/github/users.go
@@ -11,8 +11,8 @@ import (
 
 // https://docs.github.com/en/rest/users#get-a-user
 func (i integration) getUser(ctx context.Context, args []sdktypes.Value, kwargs map[string]sdktypes.Value) (sdktypes.Value, error) {
+	// Parse the input arguments.
 	var username, owner string
-
 	err := sdkmodule.UnpackArgs(args, kwargs,
 		"username", &username,
 		"owner=?", &owner,
@@ -22,19 +22,60 @@ func (i integration) getUser(ctx context.Context, args []sdktypes.Value, kwargs 
 	}
 
 	var gh *github.Client
-
 	if owner == "" {
 		// According to github docs, this endpoint requires no permissions.
 		gh, err = i.newAnonymousClient()
 	} else {
 		gh, err = i.NewClient(ctx, owner)
 	}
-
 	if err != nil {
 		return sdktypes.InvalidValue, err
 	}
 
+	// Invoke the API method.
 	resp, _, err := gh.Users.Get(ctx, username)
+	if err != nil {
+		return sdktypes.InvalidValue, err
+	}
+
+	return sdktypes.WrapValue(resp)
+}
+
+// https://docs.github.com/en/rest/search/search#search-users
+// https://docs.github.com/en/search-github/searching-on-github/searching-users
+func (i integration) searchUsers(ctx context.Context, args []sdktypes.Value, kwargs map[string]sdktypes.Value) (sdktypes.Value, error) {
+	// Parse the input arguments.
+	var (
+		query, owner string
+
+		opts github.SearchOptions
+	)
+	err := sdkmodule.UnpackArgs(args, kwargs,
+		"query", &query,
+
+		"sort?", &opts.Sort,
+		"order?", &opts.Order,
+		"per_page?", &opts.PerPage,
+		"page?", &opts.Page,
+		"owner=?", &owner,
+	)
+	if err != nil {
+		return sdktypes.InvalidValue, err
+	}
+
+	var gh *github.Client
+	if owner == "" {
+		// According to github docs, this endpoint requires no permissions.
+		gh, err = i.newAnonymousClient()
+	} else {
+		gh, err = i.NewClient(ctx, owner)
+	}
+	if err != nil {
+		return sdktypes.InvalidValue, err
+	}
+
+	// Invoke the API method.
+	resp, _, err := gh.Search.Users(ctx, query, &opts)
 	if err != nil {
 		return sdktypes.InvalidValue, err
 	}
