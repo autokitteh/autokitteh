b8e404e07ee3f4b578593ba09624ddda8f680557
Efi Shtain <efi@autokitteh.com>
2024-05-19 17:16:46 +0300

    verify order in backend grpc endpoint

--- internal/backend/eventsgrpcsvc/svc.go
+++ internal/backend/eventsgrpcsvc/svc.go
@@ -71,11 +71,26 @@ func (s *server) List(ctx context.Context, req *connect.Request[eventsv1.ListReq
 	if err != nil {
 		return nil, sdkerrors.AsConnectError(err)
 	}
+
+	// set default order if not set
+	if msg.Order == "" {
+		msg.Order = sdkservices.ListOrderDescending
+	}
+
+	// verify order is valid
+	if msg.Order != sdkservices.ListOrderAscending && msg.Order != sdkservices.ListOrderDescending {
+		return nil, connect.NewError(
+			connect.CodeInvalidArgument,
+			fmt.Errorf("order should be either %s or %s", sdkservices.ListOrderAscending, sdkservices.ListOrderDescending),
+		)
+	}
+
 	filter := sdkservices.ListEventsFilter{
 		IntegrationID: iid,
 		ConnectionID:  cid,
 		EventType:     msg.EventType,
 		Limit:         int(msg.MaxResults),
+		Order:         msg.Order,
 	}
 
 	events, err := s.events.List(ctx, filter)
