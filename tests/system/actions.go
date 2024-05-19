c0020a4041ec793d920bc59ffeb099867b44a843
Igor Rayak <igor@autokitteh.com>
2024-05-19 13:23:05 +0300

    better args splitting for ak tests

--- tests/system/actions.go
+++ tests/system/actions.go
@@ -2,6 +2,7 @@ package systest
 
 import (
 	"errors"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -13,13 +14,24 @@ type akResult struct {
 	returnCode int
 }
 
+func splitToArgs(cmdArgs string) []string {
+	re := regexp.MustCompile(`\w+=".*?"|\w+=[\w.:]+|"[^"]+"|[\w-.:/]+`)
+	args := re.FindAllString(cmdArgs, -1)
+	for i, arg := range args {
+		if len(arg) >= 1 && arg[0] == '"' && arg[len(arg)-1] == '"' {
+			args[i] = strings.Trim(arg, "\"")
+		}
+	}
+	return args
+}
+
 func runAction(t *testing.T, akPath, akAddr, step string) (any, error) {
 	match := actions.FindStringSubmatch(step)
 	switch match[1] {
 	case "setenv":
 		return nil, setEnv(strings.TrimSpace(strings.TrimPrefix(step, match[1])))
 	case "ak":
-		args := append(config.ServiceUrlArg(akAddr), strings.Fields(match[3])...)
+		args := append(config.ServiceUrlArg(akAddr), splitToArgs(match[3])...)
 		return runClient(akPath, args)
 	case "http get", "http post":
 		method := strings.ToUpper(match[2])
