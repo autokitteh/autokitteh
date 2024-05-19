c63f296a32468c9bfdbd8ffdca0763fd58303d87
Igor Rayak <igor@autokitteh.com>
2024-05-19 13:23:05 +0300

    respect quptes while splitting args

--- tests/system/actions.go
+++ tests/system/actions.go
@@ -2,6 +2,7 @@ package systest
 
 import (
 	"errors"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -13,13 +14,25 @@ type akResult struct {
 	returnCode int
 }
 
+func splitToArgs(cmdArgs string) []string {
+	// unfortunaly string.Fields will split everything by spaces, including quoted args. Let's use regexp instead
+	// re := regexp.MustCompile(`[^\s"']+|([^\s"']*"([^"]*)"[^\s"']*)+|'([^']*)`). DO we need quotes in arg?
+	re := regexp.MustCompile(`[^\s"']+|"[^"]*"|'[^']*'`)
+	args := re.FindAllString(cmdArgs, -1)
+	for i, arg := range args {
+		arg = strings.Trim(arg, "\"'")
+		args[i] = arg
+	}
+	return args
+}
+
 func runAction(t *testing.T, akPath, akAddr, step string) (any, error) {
 	match := actions.FindStringSubmatch(step)
 	switch match[1] {
 	case "setenv":
 		return nil, setEnv(strings.TrimSpace(strings.TrimPrefix(step, match[1])))
 	case "ak":
-		args := append(config.ServiceUrlArg(akAddr), strings.Fields(match[3])...)
+		args := append(config.ServiceUrlArg(akAddr), splitToArgs(match[3])...)
 		return runClient(akPath, args)
 	case "http get", "http post":
 		method := strings.ToUpper(match[2])
