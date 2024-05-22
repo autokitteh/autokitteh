fe6a52db0684a2f457895fa9c4d4afa26b247683
Igor Rayak <igor@autokitteh.com>
2024-05-22 15:17:04 +0300

    tests

---
+++ tests/system/actions_test.go
@@ -0,0 +1,41 @@
+package systest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitToArgs(t *testing.T) {
+	testCases := []struct {
+		cmdArgs  string
+		expected []string
+	}{
+		{
+			cmdArgs:  `simple args1 x 1`,
+			expected: []string{"simple", "args1", "x", "1"},
+		},
+		{
+			// test [.:]
+			cmdArgs:  `session start --build-id bld_03 --entrypoint main.star:main "`,
+			expected: []string{"session", "start", "--build-id", "bld_03", "--entrypoint", "main.star:main"},
+		},
+		{
+			// test x=y, with int, dot, quotes
+			cmdArgs:  `pass --input a=1 --input b=2.3 --input c="meow"`,
+			expected: []string{"pass", "--input", "a=1", "--input", "b=2.3", "--input", `c="meow"`},
+		},
+		{
+			// test schedule
+			cmdArgs:  `--schedule1 "* * * * *" --schedule2 "0 0 */1 * *" --schedule3 "@every 1h3m2s"`,
+			expected: []string{"--schedule1", "* * * * *", "--schedule2", "0 0 */1 * *", "--schedule3", "@every 1h3m2s"},
+		},
+	}
+
+	for _, tc := range testCases {
+		result := splitToArgs(tc.cmdArgs)
+
+		assert.Equal(t, len(tc.expected), len(result))
+		assert.Equal(t, tc.expected, result)
+	}
+}
