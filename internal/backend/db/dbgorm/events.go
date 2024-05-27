6091d5f4fb1214960a383789f7c3f13a48f92087
Itay Donanhirsh <itay@autokitteh.com>
2024-05-27 01:07:06 -0700

    omit

--- internal/backend/db/dbgorm/events.go
+++ internal/backend/db/dbgorm/events.go
@@ -92,7 +92,7 @@ func (db *gormdb) ListEvents(ctx context.Context, filter sdkservices.ListEventsF
 	}
 
 	var es []scheme.Event
-	if err := q.Select(scheme.EventColNamesWithoutData).Find(&es).Error; err != nil {
+	if err := q.Omit("data", "compressed_data").Find(&es).Error; err != nil {
 		return nil, translateError(err)
 	}
 
