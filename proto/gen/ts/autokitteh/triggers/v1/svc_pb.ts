3cae290e2b703af889336719c23d6733001fed4c
Igor Rayak <igor@autokitteh.com>
2024-05-28 15:44:33 +0300

    add project_id to trigger list proto

--- proto/gen/ts/autokitteh/triggers/v1/svc_pb.ts
+++ proto/gen/ts/autokitteh/triggers/v1/svc_pb.ts
@@ -305,6 +305,11 @@ export class ListRequest extends Message<ListRequest> {
    */
   connectionId = "";
 
+  /**
+   * @generated from field: string project_id = 3;
+   */
+  projectId = "";
+
   constructor(data?: PartialMessage<ListRequest>) {
     super();
     proto3.util.initPartial(data, this);
@@ -315,6 +320,7 @@ export class ListRequest extends Message<ListRequest> {
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
     { no: 1, name: "env_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
     { no: 2, name: "connection_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
+    { no: 3, name: "project_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): ListRequest {
