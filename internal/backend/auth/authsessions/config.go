1df5167b72b023846f4535dc88393bb3b5ca3b7e
efi shtain <efi@autokitteh.com>
2024-05-28 09:11:06 +0300

    ENG-927: set prod cookie (#340)
    - remove cookie config - was impossible to set
    - set correct defaults for cross site usage

--- internal/backend/auth/authsessions/config.go
+++ internal/backend/auth/authsessions/config.go
@@ -1,26 +1,26 @@
 package authsessions
 
 import (
+	"net/http"
+
 	"github.com/dghubble/sessions"
 
 	"go.autokitteh.dev/autokitteh/internal/backend/configset"
 )
 
 type Config struct {
-	Cookie     *sessions.CookieConfig `koanf:"cookie"`
-	CookieKeys string                 `koanf:"cookie_keys"` // pairs of hash and block keys.
-}
-
-func (c Config) cookieConfig() *sessions.CookieConfig {
-	if c.Cookie == nil {
-		return sessions.DefaultCookieConfig
-	}
-
-	return c.Cookie
+	Cookie     *sessions.CookieConfig
+	CookieKeys string `koanf:"cookie_keys"` // pairs of hash and block keys.
 }
 
 var Configs = configset.Set[Config]{
-	Default: &Config{},
+	Default: &Config{
+		Cookie: func() *sessions.CookieConfig {
+			c := sessions.DefaultCookieConfig
+			c.SameSite = http.SameSiteNoneMode
+			return c
+		}(),
+	},
 	Dev: &Config{
 		Cookie:     sessions.DebugCookieConfig,
 		CookieKeys: "0000000000000000000000000000000000000000000000000000000000000000,0000000000000000000000000000000000000000000000000000000000000000",
