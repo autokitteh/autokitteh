e72cb4e734e427df29e1c8d47bcdd3f662038aad
Igor Rayak <igor@autokitteh.com>
2024-06-04 12:36:51 +0300

    add Note

--- sdk/sdktypes/value_wrap.go
+++ sdk/sdktypes/value_wrap.go
@@ -89,6 +89,7 @@ func (w ValueWrapper) Wrap(v any) (Value, error) {
 
 			if vfs.Anonymous && vfs.Type != reflect.TypeOf(time.Time{}) {
 				// skip anonymous but allow to wrap embedded time.Time (see tests)
+				// as a most common and problematic case (e.g. github Go API Timestamp)
 				continue
 			}
 
