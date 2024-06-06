89d6f72c04152d2d9ecac521b2bc9888be695284
Miki Tebeka <miki@autokitteh.com>
2024-06-06 17:10:57 +0300

    events workflow

---
+++ runtimes/pythonrt/testdata/events/events.py
@@ -0,0 +1,23 @@
+import autokitteh as ak
+
+
+def on_http_get():
+    print("creating subscription")
+    postSubscription = ak.subscribe('http_event', 'data.url.path == "/test" && data.method == "POST"')
+    getSubscription = ak.subscribe('http_event', 'data.url.path == "/test" && data.method == "GET"')
+
+    print("waiting for event on post subscription")
+    print(postSubscription)
+    event = ak.next_event(postSubscription)
+    print("got event")
+    print(event)
+    print("waiting for event on post or get")
+    print(postSubscription)
+    print(getSubscription)
+    event = ak.next_event(getSubscription, postSubscription)
+    print("got event")
+    print(event)
+
+    ak.unsubscribe(getSubscription)
+    ak.unsubscribe(postSubscription)
+    print("done")
