6280693283cbcca93cf2055c20d63cdcf0834447
Daniel Abraham <daniel@autokitteh.com>
2024-06-13 14:36:30 -0700

    Add Py sample links to Jira integration doc'n

--- integrations/jira/client.go
+++ integrations/jira/client.go
@@ -23,9 +23,11 @@ var desc = kittehs.Must1(sdktypes.StrictIntegrationFromProto(&sdktypes.Integrati
 	Description:   "Atlassian Jira is an issue tracking and project management system.",
 	LogoUrl:       "/static/images/jira.svg",
 	UserLinks: map[string]string{
-		"1 REST API":             "https://developer.atlassian.com/cloud/jira/platform/rest/v3/intro/",
-		"2 Atlassian Python API": "https://atlassian-python-api.readthedocs.io/",
-		"3 Python Jira API":      "https://jira.readthedocs.io/",
+		"1 REST API":                  "https://developer.atlassian.com/cloud/jira/platform/rest/v3/intro/",
+		"2 Atlassian Python API":      "https://atlassian-python-api.readthedocs.io/",
+		"3 Atlassian Python examples": "https://github.com/atlassian-api/atlassian-python-api/tree/master/examples/jira",
+		"4 Python Jira API":           "https://jira.readthedocs.io/",
+		"5 Python Jira examples":      "https://github.com/pycontribs/jira/tree/main/examples",
 	},
 	ConnectionUrl: "/jira/connect/",
 	ConnectionCapabilities: &sdktypes.ConnectionCapabilitiesPB{
