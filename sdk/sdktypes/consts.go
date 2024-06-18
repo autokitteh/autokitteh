1bcdb928c0fe797d1ceeba6fdc1531db651c1a5f
Igor Rayak <igor@autokitteh.com>
2024-06-18 15:16:02 +0300

    add default user

--- sdk/sdktypes/consts.go
+++ sdk/sdktypes/consts.go
@@ -2,5 +2,8 @@ package sdktypes
 
 import "go.autokitteh.dev/autokitteh/internal/kittehs"
 
-// REVIEW: we cannot define this in fixtures, due to circular dependencies
-var BuiltinSchedulerConnectionID = kittehs.Must1(ParseConnectionID("con_3kthcr0n000000000000000000"))
+// Note: we cannot define this in fixtures, due to circular dependencies
+var (
+	BuiltinSchedulerConnectionID = kittehs.Must1(ParseConnectionID("con_3kthcr0n000000000000000000"))
+	DefaultUser                  = NewUser("ak", map[string]string{"name": "Auto Kitteh", "email": "meow@autokitteh.com"})
+)
