c9147adeb4ccfb650f46c2da968baaeadfd5ab95
Daniel Abraham <daniel@autokitteh.com>
2024-06-19 03:51:36 +0300

    ENG-1001: Support API keys and PATs in Jira (#389)

---
+++ integrations/jira/save.go
@@ -0,0 +1,46 @@
+package jira
+
+import (
+	"net/http"
+	"strings"
+
+	"go.uber.org/zap"
+
+	"go.autokitteh.dev/autokitteh/sdk/sdkintegrations"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+)
+
+const (
+	contentTypeForm = "application/x-www-form-urlencoded"
+)
+
+// handleAuth saves a new AutoKitteh connection with user-submitted data.
+func (h handler) handleSave(w http.ResponseWriter, r *http.Request) {
+	l := h.logger.With(zap.String("urlPath", r.URL.Path))
+
+	// Check the "Content-Type" header.
+	contentType := r.Header.Get(headerContentType)
+	if !strings.HasPrefix(contentType, contentTypeForm) {
+		// Probably an attack, so no need for user-friendliness.
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	// Read and parse POST request body.
+	if err := r.ParseForm(); err != nil {
+		l.Warn("Failed to parse inbound HTTP request", zap.Error(err))
+		redirectToErrorPage(w, r, "form parsing error: "+err.Error())
+		return
+	}
+
+	vars := sdktypes.NewVars().
+		Set(sdktypes.NewSymbol("BaseURL"), r.Form.Get("base_url"), true).
+		Set(sdktypes.NewSymbol("APIKeyOrPAT"), r.Form.Get("key_or_pat"), true)
+
+	email := r.Form.Get("email")
+	if email != "" {
+		vars = vars.Set(sdktypes.NewSymbol("Email"), email, true)
+	}
+
+	sdkintegrations.FinalizeConnectionInit(w, r, integrationID, vars)
+}
