e658c336af934af16e0e43b68ec0d6f7ce675ca2
Daniel Abraham <daniel@autokitteh.com>
2024-06-21 19:18:29 -0700

    Add AttrDict to SDK

--- runtimes/pythonrt/py-sdk/autokitteh/__init__.py
+++ runtimes/pythonrt/py-sdk/autokitteh/__init__.py
@@ -1,10 +1,11 @@
 """AutoKitteh Python SDK."""
 
 from . import atlassian
+from .attr_dict import AttrDict
 from .decorators import activity
 from . import errors
 from . import google
 from . import slack
 
 
-__all__ = ["activity", "atlassian", "errors", "google", "slack"]
+__all__ = ["AttrDict", "activity", "atlassian", "errors", "google", "slack"]
