72a68ba438a539b73feb08e93261fff50efc6b2f
Pasha Fateev <pasha@autokitteh.com>
2024-07-01 16:27:29 -0700

    ENG-1024: rename host to target (#439)

--- integrations/grpc/module.go
+++ integrations/grpc/module.go
@@ -53,9 +53,7 @@ func handleGenericGRPCCall() sdkexecutor.Function {
 			return sdktypes.Nothing, err
 		}
 
-		// TODO(ENG-1024): Rename to "target" here and in the sample, and
-		// reference https://github.com/grpc/grpc/blob/master/doc/naming.md
-		target, ok := args["host"].(string)
+		target, ok := args["target"].(string)
 		if !ok {
 			return sdktypes.Nothing, errors.New("host is required")
 		}
