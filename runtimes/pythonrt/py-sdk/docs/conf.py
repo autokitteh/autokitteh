31fb4bb9cf29e6e567b1404c09c5c84233b038eb
Miki Tebeka <miki@autokitteh.com>
2024-07-03 12:11:57 +0300

    format with ruff

--- runtimes/pythonrt/py-sdk/docs/conf.py
+++ runtimes/pythonrt/py-sdk/docs/conf.py
@@ -11,24 +11,23 @@ sys.path.insert(0, str(sdk_dir))  # Add the project root to the path
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'autokitteh'
-copyright = '2024, AutoKitteh'
-author = 'AutoKitteh'
-release = '0.2.5'
-
+project = "autokitteh"
+copyright = "2024, AutoKitteh"
+author = "AutoKitteh"
+release = "0.2.5"
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.napoleon']
+extensions = ["sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
