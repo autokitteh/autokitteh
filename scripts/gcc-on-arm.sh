fbfe4bc37801c29a0cc4e76ac5bb5d0a286927f0
Miki Tebeka <miki@autokitteh.com>
2024-07-07 17:22:32 +0300

    install GCC on ARM

---
+++ scripts/gcc-on-arm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Install GCC on ARM architecture, used by Docker.
+
+case $1 in
+	"-h" | "--help" ) echo "usage: $(basename $0)"; exit;;
+esac
+
+if [ $# -gt 0 ]; then
+	echo "error: wrong number of arguments" 1>&2
+	exit 1
+fi
+
+case $(uname -m) in
+	arm* )
+		apk add gcc
+		;;
+	* )
+		echo "not arm"
+		;;
+esac
