b8020ccb4476d734bb5b019e673f8eb186c5b50c
Pavel Fateev <pasha@autokitteh.com>
2024-07-10 11:25:58 -0700

    uncomment necesssary code

--- integrations/google/gemini/server.go
+++ integrations/google/gemini/server.go
@@ -8,16 +8,16 @@ import (
 	"go.autokitteh.dev/autokitteh/web/static"
 )
 
-// const (
-// 	// savePath is the URL path for our handler to save a new autokitteh
-// 	// connection, after the user submits its details via a web form.
-// 	savePath = "/googlegemini/save"
-// )
+const (
+	// savePath is the URL path for our handler to save a new autokitteh
+	// connection, after the user submits its details via a web form.
+	savePath = "/googlegemini/save"
+)
 
 func Start(l *zap.Logger, mux *http.ServeMux) {
 	// New connection UI + form submission handler.
 	uiPath := "GET " + desc.ConnectionURL().Path + "/"
 	mux.Handle(uiPath, http.FileServer(http.FS(static.ChatGPTWebContent)))
 
-	// mux.Handle("POST "+savePath, NewHTTPHandler(l))
+	mux.Handle("POST "+savePath, NewHTTPHandler(l))
 }
