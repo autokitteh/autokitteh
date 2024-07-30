c43f0afaaff98a53a4ba58af165c43a40bf1c62e
Igor Rayak <igor@autokitteh.com>
2024-07-30 22:39:39 +0300

    fix: pass authMux to google integration

--- integrations/google/server.go
+++ integrations/google/server.go
@@ -24,35 +24,35 @@ const (
 	gmailWebhookPath = "/gmail/notif"
 )
 
-func Start(l *zap.Logger, mux *http.ServeMux, v sdkservices.Vars, o sdkservices.OAuth, d sdkservices.Dispatcher) {
+func Start(l *zap.Logger, muxNoAuth *http.ServeMux, muxAuth *http.ServeMux, v sdkservices.Vars, o sdkservices.OAuth, d sdkservices.Dispatcher) {
 	uiPath := "GET " + desc.ConnectionURL().Path + "/"
 
 	// New connection UIs + handlers.
-	mux.Handle(uiPath, http.FileServer(http.FS(static.GoogleWebContent)))
+	muxNoAuth.Handle(uiPath, http.FileServer(http.FS(static.GoogleWebContent)))
 
 	urlPath := strings.ReplaceAll(uiPath, "google", "gmail")
-	mux.Handle(urlPath, http.FileServer(http.FS(static.GmailWebContent)))
+	muxNoAuth.Handle(urlPath, http.FileServer(http.FS(static.GmailWebContent)))
 
 	urlPath = strings.ReplaceAll(uiPath, "google", "googlecalendar")
-	mux.Handle(urlPath, http.FileServer(http.FS(static.GoogleCalendarWebContent)))
+	muxNoAuth.Handle(urlPath, http.FileServer(http.FS(static.GoogleCalendarWebContent)))
 
 	urlPath = strings.ReplaceAll(uiPath, "google", "googlechat")
-	mux.Handle(urlPath, http.FileServer(http.FS(static.GoogleChatWebContent)))
+	muxNoAuth.Handle(urlPath, http.FileServer(http.FS(static.GoogleChatWebContent)))
 
 	urlPath = strings.ReplaceAll(uiPath, "google", "googledrive")
-	mux.Handle(urlPath, http.FileServer(http.FS(static.GoogleDriveWebContent)))
+	muxNoAuth.Handle(urlPath, http.FileServer(http.FS(static.GoogleDriveWebContent)))
 
 	urlPath = strings.ReplaceAll(uiPath, "google", "googleforms")
-	mux.Handle(urlPath, http.FileServer(http.FS(static.GoogleFormsWebContent)))
+	muxNoAuth.Handle(urlPath, http.FileServer(http.FS(static.GoogleFormsWebContent)))
 
 	urlPath = strings.ReplaceAll(uiPath, "google", "googlesheets")
-	mux.Handle(urlPath, http.FileServer(http.FS(static.GoogleSheetsWebContent)))
+	muxNoAuth.Handle(urlPath, http.FileServer(http.FS(static.GoogleSheetsWebContent)))
 
 	h := NewHTTPHandler(l, o, v, d)
-	mux.HandleFunc("GET "+oauthPath, h.handleOAuth)
-	mux.HandleFunc("POST "+credsPath, h.handleCreds)
+	muxAuth.HandleFunc("GET "+oauthPath, h.handleOAuth)
+	muxNoAuth.HandleFunc("POST "+credsPath, h.handleCreds)
 
 	// Event webhooks.
-	mux.HandleFunc("POST "+formsWebhookPath, h.handleFormsNotification)
-	mux.HandleFunc("POST "+gmailWebhookPath, h.handleGmailNotification)
+	muxNoAuth.HandleFunc("POST "+formsWebhookPath, h.handleFormsNotification)
+	muxNoAuth.HandleFunc("POST "+gmailWebhookPath, h.handleGmailNotification)
 }
