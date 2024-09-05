12a907e0bfb8da244283ef3d90d511fdad79c221
Itay Donanhirsh <itay@autokitteh.com>
2024-09-04 19:12:18 -0700

    efi

--- migrations/postgres/20240904081936_trigger-dests.sql
+++ migrations/postgres/20240904081936_trigger-dests.sql
@@ -6,8 +6,12 @@ CREATE INDEX "idx_triggers_source_type" ON "triggers" ("source_type");
 -- create index "idx_triggers_webhook_slug" to table: "triggers"
 CREATE INDEX "idx_triggers_webhook_slug" ON "triggers" ("webhook_slug");
 -- modify "events" table
-ALTER TABLE "events" ADD COLUMN "destination_id" uuid NOT NULL, ADD COLUMN "trigger_id" uuid NULL, ADD
+ALTER TABLE "events" ADD COLUMN "destination_id" uuid, ADD COLUMN "trigger_id" uuid NULL, ADD
  CONSTRAINT "fk_events_trigger" FOREIGN KEY ("trigger_id") REFERENCES "triggers" ("trigger_id") ON UPDATE NO ACTION ON DELETE NO ACTION;
+-- copy connection_id to new trigger_id column
+UPDATE "events" SET destination_id=connection_id;
+-- modify "events" table
+ALTER TABLE "events" ALTER COLUMN "destination_id" SET NOT NULL;
 -- create index "idx_events_destination_id" to table: "events"
 CREATE INDEX "idx_events_destination_id" ON "events" ("destination_id");
 -- create index "idx_events_trigger_id" to table: "events"
@@ -15,8 +19,12 @@ CREATE INDEX "idx_events_trigger_id" ON "events" ("trigger_id");
 -- drop index "idx_connection_id_event_type" from table: "signals"
 DROP INDEX "idx_connection_id_event_type";
 -- modify "signals" table
-ALTER TABLE "signals" ALTER COLUMN "signal_id" TYPE uuid, ALTER COLUMN "connection_id" DROP NOT NULL, ADD COLUMN "destination_id" uuid NOT NULL, ADD COLUMN "trigger_id" uuid NULL, ADD
+ALTER TABLE "signals" ALTER COLUMN "signal_id" TYPE uuid using signal_id::uuid, ALTER COLUMN "connection_id" DROP NOT NULL, ADD COLUMN "destination_id" uuid, ADD COLUMN "trigger_id" uuid NULL, ADD
  CONSTRAINT "fk_signals_trigger" FOREIGN KEY ("trigger_id") REFERENCES "triggers" ("trigger_id") ON UPDATE NO ACTION ON DELETE NO ACTION;
+-- copy connection_id to destination_id on signals
+UPDATE "signals" SET destination_id=connection_id;
+-- modify "signals" table
+ALTER TABLE "signals" ALTER COLUMN "destination_id" SET NOT NULL;
 -- create index "idx_signals_destination_id" to table: "signals"
 CREATE INDEX "idx_signals_destination_id" ON "signals" ("destination_id");
 
