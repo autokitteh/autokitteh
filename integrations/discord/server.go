15f76f54c02339fe9ae7aa57eb21695cbaf908df
Pasha Fateev <pasha@autokitteh.com>
2024-09-04 19:06:25 -0700

    feat(integrations): add event handling for Discord integration (#627)
    **refs:**
    - #618 (closed because of signing issue)
    - eng-1226

--- integrations/discord/server.go
+++ integrations/discord/server.go
@@ -1,11 +1,15 @@
 package discord
 
 import (
+	"context"
 	"net/http"
 
 	"go.uber.org/zap"
 
+	"go.autokitteh.dev/autokitteh/integrations/discord/internal/vars"
 	"go.autokitteh.dev/autokitteh/internal/backend/muxes"
+	"go.autokitteh.dev/autokitteh/sdk/sdkservices"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
 	"go.autokitteh.dev/autokitteh/web/static"
 )
 
@@ -17,7 +21,7 @@ const (
 
 // Start initializes all the HTTP handlers of the Discord integration.
 // This includes connection UIs, initialization webhooks, and event webhooks.
-func Start(l *zap.Logger, muxes *muxes.Muxes) {
+func Start(l *zap.Logger, muxes *muxes.Muxes, v sdkservices.Vars, d sdkservices.Dispatcher) {
 	// Connection UI.
 	uiPath := "GET " + desc.ConnectionURL().Path + "/"
 	muxes.NoAuth.Handle(uiPath, http.FileServer(http.FS(static.DiscordWebContent)))
@@ -26,7 +30,24 @@ func Start(l *zap.Logger, muxes *muxes.Muxes) {
 	// to have an authenticated user context, so the DB layer won't reject them.
 	// For this purpose, init webhooks are managed by the "auth" mux, which passes
 	// through AutoKitteh's auth middleware to extract the user ID from a cookie.
-	muxes.Auth.Handle("POST "+savePath, NewHTTPHandler(l))
+	wsh := NewHandler(l, v, d, desc)
 
-	// TODO(ENG-1226): Event webhooks (unauthenticated by definition).
+	muxes.Auth.Handle("POST "+savePath, wsh)
+
+	// Initialize WebSocket pool.
+	cids, err := v.FindConnectionIDs(context.Background(), integrationID, vars.BotToken, "")
+	if err != nil {
+		l.Error("Failed to list WebSocket-based connection IDs", zap.Error(err))
+		return
+	}
+
+	for _, cid := range cids {
+		data, err := v.Get(context.Background(), sdktypes.NewVarScopeID(cid))
+		if err != nil {
+			l.Error("Missing data for Discord WebSocket connection", zap.Error(err))
+			continue
+		}
+
+		wsh.OpenWebSocketConnection(data.GetValue(vars.BotToken))
+	}
 }
---
+++ integrations/discord/websocket.go
@@ -0,0 +1,101 @@
+package discord
+
+import (
+	"context"
+
+	"github.com/bwmarrin/discordgo"
+	"go.uber.org/zap"
+
+	"go.autokitteh.dev/autokitteh/integrations/internal/extrazap"
+	"go.autokitteh.dev/autokitteh/internal/kittehs"
+	"go.autokitteh.dev/autokitteh/sdk/sdkservices"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+)
+
+type handler struct {
+	logger        *zap.Logger
+	vars          sdkservices.Vars
+	dispatcher    sdkservices.Dispatcher
+	integrationID sdktypes.IntegrationID
+}
+
+func NewHandler(l *zap.Logger, v sdkservices.Vars, d sdkservices.Dispatcher, i sdktypes.Integration) handler {
+	return handler{
+		logger:        l,
+		vars:          v,
+		dispatcher:    d,
+		integrationID: i.ID(),
+	}
+}
+
+func (h handler) OpenWebSocketConnection(botToken string) {
+	dg, err := discordgo.New("Bot " + botToken)
+	if err != nil {
+		h.logger.Error("Error creating Discord session", zap.Error(err))
+		return
+	}
+
+	h.addHandlers(dg)
+
+	// Open a WebSocket connection to Discord.
+	if err := dg.Open(); err == nil {
+		return // Connection opened successfully, normal termination.
+	}
+	h.logger.Error("Failed to open Discord WebSocket connection", zap.Error(err))
+}
+
+func (h handler) dispatchAsyncEventsToConnections(cids []sdktypes.ConnectionID, e sdktypes.Event) {
+	ctx := extrazap.AttachLoggerToContext(h.logger, context.Background())
+	for _, cid := range cids {
+		eid, err := h.dispatcher.Dispatch(ctx, e.WithConnectionID(cid), nil)
+		l := h.logger.With(
+			zap.String("connectionID", cid.String()),
+			zap.String("eventID", eid.String()),
+		)
+		if err != nil {
+			l.Error("Event dispatch failed", zap.Error(err))
+			return
+		}
+		l.Debug("Event dispatched")
+	}
+}
+
+// transformEvent transforms the received Discord event into an AutoKitteh event.
+func (h handler) transformEvent(discordEvent any, eventType string) (sdktypes.Event, error) {
+	l := h.logger.With(
+		zap.String("eventType", eventType),
+		zap.Any("event", discordEvent),
+	)
+
+	wrapped, err := sdktypes.WrapValue(discordEvent)
+	if err != nil {
+		l.Error("Failed to wrap Discord event", zap.Error(err))
+		return sdktypes.InvalidEvent, err
+	}
+
+	data, err := wrapped.ToStringValuesMap()
+	if err != nil {
+		l.Error("Failed to convert wrapped Discord event", zap.Error(err))
+		return sdktypes.InvalidEvent, err
+	}
+
+	akEvent, err := sdktypes.EventFromProto(&sdktypes.EventPB{
+		EventType: eventType,
+		Data:      kittehs.TransformMapValues(data, sdktypes.ToProto),
+	})
+	if err != nil {
+		l.Error("Failed to convert protocol buffer to SDK event",
+			zap.Any("data", data),
+			zap.Error(err),
+		)
+		return sdktypes.InvalidEvent, err
+	}
+
+	return akEvent, nil
+}
+
+func (h handler) addHandlers(dg *discordgo.Session) {
+	dg.AddHandler(h.handleMessageCreate)
+	dg.AddHandler(h.handleMessageDelete)
+	dg.AddHandler(h.handleMessageUpdate)
+}
