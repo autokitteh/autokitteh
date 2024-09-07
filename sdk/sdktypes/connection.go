cce3a7cb832f066b20cddda6ff262ba3653cacd1
Itay Donanhirsh <itay@autokitteh.com>
2024-09-06 17:50:07 -0700

    fix

--- sdk/sdktypes/connection.go
+++ sdk/sdktypes/connection.go
@@ -28,7 +28,11 @@ func (ConnectionTraits) Validate(m *ConnectionPB) error {
 }
 
 func (ConnectionTraits) StrictValidate(m *ConnectionPB) error {
-	return errors.Join(mandatory("name", m.Name))
+	return errors.Join(
+		mandatory("name", m.Name),
+		mandatory("project_id", m.ProjectId),
+		mandatory("integration_id", m.IntegrationId),
+	)
 }
 
 func ConnectionFromProto(m *ConnectionPB) (Connection, error) { return FromProto[Connection](m) }
