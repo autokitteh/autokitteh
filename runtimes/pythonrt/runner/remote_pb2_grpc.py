2aa3fbf83fed78270d52f84c12510ab8c1cd4f4f
Efi Shtain <efi@autokitteh.com>
2024-09-16 17:22:05 +0300

    upgrade proto deps

--- runtimes/pythonrt/runner/remote_pb2_grpc.py
+++ runtimes/pythonrt/runner/remote_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import remote_pb2 as remote__pb2
 
-GRPC_GENERATED_VERSION = '1.66.0'
+GRPC_GENERATED_VERSION = '1.66.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
