32cefd79a5e3975584a3469568be5d985c9ae0f3
Efi Shtain <efi@autokitteh.com>
2024-09-24 18:37:25 +0300

    debug log print error

--- internal/backend/sessions/sessionworkflows/workflow.go
+++ internal/backend/sessions/sessionworkflows/workflow.go
@@ -498,7 +498,7 @@ func (w *sessionWorkflow) run(wctx workflow.Context, l *zap.Logger) (prints []st
 			// We do not consider print failure as a critical error, since we don't want to hold back the
 			// workflow for potential user debugging prints. Just log the error and move on.
 			if err != nil {
-				sl.With("err", err).Errorf("failed to add print session record: %v", err)
+				sl.With("err", err).Debugf("failed to add print session record: %v", err)
 			}
 		},
 	}
