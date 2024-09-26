eeae00821b89c002d38790efb607c90c0c9754eb
Pasha Fateev <pasha@autokitteh.com>
2024-09-26 16:11:02 -0700

    feat(integrations): add basic asana integration with pat (#694)

---
+++ integrations/asana/client.go
@@ -0,0 +1,119 @@
+package asana
+
+import (
+	"context"
+	"io"
+	"net/http"
+
+	"go.autokitteh.dev/autokitteh/integrations"
+	"go.autokitteh.dev/autokitteh/internal/kittehs"
+	"go.autokitteh.dev/autokitteh/sdk/sdkintegrations"
+	"go.autokitteh.dev/autokitteh/sdk/sdkmodule"
+	"go.autokitteh.dev/autokitteh/sdk/sdkservices"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+)
+
+type integration struct{ vars sdkservices.Vars }
+
+var (
+	pat           = sdktypes.NewSymbol("pat")
+	authType      = sdktypes.NewSymbol("authType")
+	integrationID = sdktypes.NewIntegrationIDFromName("asana")
+)
+
+var desc = kittehs.Must1(sdktypes.StrictIntegrationFromProto(&sdktypes.IntegrationPB{
+	IntegrationId: integrationID.String(),
+	UniqueName:    "asana",
+	DisplayName:   "Asana",
+	Description:   "Asana is a web and mobile application designed to help teams organize, track, and manage their work.",
+	LogoUrl:       "/static/images/asana.svg",
+	UserLinks: map[string]string{
+		"Asana developer platform": "https://developers.asana.com/",
+	},
+	ConnectionUrl: "/asana/connect",
+	ConnectionCapabilities: &sdktypes.ConnectionCapabilitiesPB{
+		RequiresConnectionInit: true,
+	},
+}))
+
+func New(cvars sdkservices.Vars) sdkservices.Integration {
+	i := &integration{vars: cvars}
+	return sdkintegrations.NewIntegration(
+		desc,
+		sdkmodule.New(),
+		connStatus(i),
+		connTest(i),
+		sdkintegrations.WithConnectionConfigFromVars(cvars),
+	)
+}
+
+// connStatus is an optional connection status check provided by
+// the integration to AutoKitteh. The possible results are "Init
+// required" (the connection is not usable yet) and "Initialized".
+func connStatus(i *integration) sdkintegrations.OptFn {
+	return sdkintegrations.WithConnectionStatus(func(ctx context.Context, cid sdktypes.ConnectionID) (sdktypes.Status, error) {
+		if !cid.IsValid() {
+			return sdktypes.NewStatus(sdktypes.StatusCodeWarning, "Init required"), nil
+		}
+
+		vs, err := i.vars.Get(ctx, sdktypes.NewVarScopeID(cid))
+		if err != nil {
+			return sdktypes.InvalidStatus, err
+		}
+
+		at := vs.Get(authType)
+		if !at.IsValid() || at.Value() == "" {
+			return sdktypes.NewStatus(sdktypes.StatusCodeWarning, "Init required"), nil
+		}
+
+		switch at.Value() {
+		case integrations.OAuth:
+			return sdktypes.NewStatus(sdktypes.StatusCodeOK, "Using Asana app"), nil
+		case integrations.PAT:
+			return sdktypes.NewStatus(sdktypes.StatusCodeOK, "Using PAT"), nil
+		default:
+			return sdktypes.NewStatus(sdktypes.StatusCodeError, "Bad auth type"), nil
+		}
+	})
+}
+
+// connTest is an optional connection test provided by the integration
+// to AutoKitteh. It is used to verify that the connection is working
+// as expected. The possible results are "OK" and "error".
+func connTest(i *integration) sdkintegrations.OptFn {
+	return sdkintegrations.WithConnectionTest(func(ctx context.Context, cid sdktypes.ConnectionID) (sdktypes.Status, error) {
+		if !cid.IsValid() {
+			return sdktypes.NewStatus(sdktypes.StatusCodeWarning, "Init required"), nil
+		}
+
+		vs, err := i.vars.Get(ctx, sdktypes.NewVarScopeID(cid))
+		if err != nil {
+			return sdktypes.InvalidStatus, err
+		}
+
+		req, err := http.NewRequest("GET", "https://app.asana.com/api/1.0/users/me", nil)
+		if err != nil {
+			return sdktypes.InvalidStatus, err
+		}
+
+		pat := vs.Get(pat).Value()
+		req.Header.Add("Authorization", "Bearer "+pat)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return sdktypes.NewStatus(sdktypes.StatusCodeError, err.Error()), nil
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return sdktypes.NewStatus(sdktypes.StatusCodeError, err.Error()), nil
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return sdktypes.NewStatus(sdktypes.StatusCodeError, string(body)), nil
+		}
+
+		return sdktypes.NewStatus(sdktypes.StatusCodeOK, ""), nil
+	})
+}
---
+++ integrations/asana/save.go
@@ -0,0 +1,90 @@
+package asana
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"go.autokitteh.dev/autokitteh/integrations"
+	"go.autokitteh.dev/autokitteh/sdk/sdkintegrations"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+	"go.uber.org/zap"
+)
+
+const (
+	headerContentType = "Content-Type"
+	contentTypeForm   = "application/x-www-form-urlencoded"
+)
+
+// handler is an autokitteh webhook which implements [http.Handler]
+// to save data from web form submissions as connections.
+type handler struct {
+	logger *zap.Logger
+}
+
+func NewHTTPHandler(l *zap.Logger) http.Handler {
+	return handler{logger: l}
+}
+
+// ServeHTTP saves a new autokitteh connection with user-submitted data.
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c, l := sdkintegrations.NewConnectionInit(h.logger, w, r, desc)
+
+	// Check "Content-Type" header.
+	contentType := r.Header.Get(headerContentType)
+	if !strings.HasPrefix(contentType, contentTypeForm) {
+		c.AbortBadRequest("unexpected content type")
+		return
+	}
+
+	// Read and parse POST request body.
+	if err := r.ParseForm(); err != nil {
+		l.Warn("Failed to parse incoming HTTP request", zap.Error(err))
+		c.AbortBadRequest("form parsing error")
+		return
+	}
+
+	// Test the PAT's usability
+	req, err := http.NewRequest("GET", "https://app.asana.com/api/1.0/users/me", nil)
+	if err != nil {
+		l.Error("Failed to create HTTP request", zap.Error(err))
+		c.AbortServerError("request creation error")
+		return
+	}
+
+	ps := r.Form.Get("pat")
+	if ps == "" {
+		l.Warn("PAT not provided in request form")
+		c.AbortBadRequest("PAT is missing")
+		return
+	}
+
+	req.Header.Add("Authorization", "Bearer "+ps)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		l.Error("Failed to execute HTTP request", zap.Error(err))
+		c.AbortBadRequest("execution error")
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		l.Error("Failed to read response body", zap.Error(err))
+		c.AbortServerError("response reading error")
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		l.Warn("Token is invalid or an error occurred", zap.Int("status_code", resp.StatusCode))
+		c.AbortBadRequest("invalid token or error occurred")
+		return
+	}
+
+	// Finalize the connection with the valid PAT
+	c.Finalize(sdktypes.NewVars().
+		Set(pat, ps, true).
+		Set(authType, integrations.Init, false))
+}
