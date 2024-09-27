2f3098ce671df62addee103d3ee6fdf05434ea69
Daniel Abraham <daniel@autokitteh.com>
2024-09-27 13:11:49 -0700

    visit imports

--- runtimes/pythonrt/ak_runner/loader.py
+++ runtimes/pythonrt/ak_runner/loader.py
@@ -25,22 +25,48 @@ class Transformer(ast.NodeTransformer):
 
     def visit_Call(self, node):
         # Recurse, see https://docs.python.org/3/library/ast.html#ast.NodeVisitor.generic_visit
+        # and https://docs.python.org/3/library/ast.html#ast.NodeTransformer
         self.generic_visit(node)
 
         name = name_of(node.func, self.code_lines)
-        print(str(node))
-        print(str(name))
+        print(f"CALL LINE: {self.code_lines[node.lineno - 1]}")
+        print(f"CALL NAME: {name}")
 
         if not name or name in BUILTIN:
             return node
 
         log.info("%s:%d: patching %s with action", self.file_name, node.lineno, name)
+        print(f"PATCHING CALL IN {self.file_name}:{node.lineno}: {name}")
         call = ast.Call(
             func=ast.Name(id=ACTION_NAME, ctx=ast.Load()),
             args=[node.func] + node.args,
             keywords=node.keywords,
         )
         return call
+
+    def visit_Import(self, node: ast.Import):
+        # Recurse, see https://docs.python.org/3/library/ast.html#ast.NodeVisitor.generic_visit
+        # and https://docs.python.org/3/library/ast.html#ast.NodeTransformer
+        self.generic_visit(node)
+
+        for alias in node.names:
+            print(f"IMPORT: ALIAS NAME {alias.name}")
+            # self._parse_module(module_name)
+
+        return node
+
+    def visit_ImportFrom(self, node: ast.ImportFrom):
+        # Recurse, see https://docs.python.org/3/library/ast.html#ast.NodeVisitor.generic_visit
+        # and https://docs.python.org/3/library/ast.html#ast.NodeTransformer
+        self.generic_visit(node)
+
+        print(f"IMPORT FROM: MODULE {node.module}")
+        for alias in node.names:
+            print(f"IMPORT FROM: ALIAS NAME {alias.name}")
+            # self._parse_module(module_name)
+
+        # self._parse_module(module_name)
+        return node
 
 
 def load_code(root_path, action_fn, module_name):
