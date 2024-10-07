08cab1ef8160d920508b0490449caa90053ac4b8
Miki Tebeka <miki@autokitteh.com>
2024-10-07 18:16:39 +0300

    done?

--- cmd/ak/cmd/export/export.go
+++ cmd/ak/cmd/export/export.go
@@ -1,9 +1,11 @@
 package export
 
 import (
+	"bytes"
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 
 	"go.autokitteh.dev/autokitteh/cmd/ak/common"
 	"go.autokitteh.dev/autokitteh/internal/resolver"
@@ -46,20 +48,57 @@ func export(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	fmt.Println("version: v1")
-	fmt.Println()
-	fmt.Println("project:")
-	fmt.Printf("  name: %s\n", prj.Name().String())
-	fmt.Println("  triggers:")
+	// We print to buffer until the end so in case of errors we won't print anything but the error
+	var buf bytes.Buffer
+	fmt.Fprintln(&buf, "version: v1")
+	fmt.Fprintln(&buf)
+	fmt.Fprintln(&buf, "project:")
+	fmt.Fprintf(&buf, "  name: %s\n", prj.Name().String())
+
+	cf := sdkservices.ListConnectionsFilter{
+		ProjectID: prj.ID(),
+	}
+	conns, err := r.Client.Connections().List(ctx, cf)
+	if err != nil {
+		return err
+	}
+
+	if len(conns) > 0 {
+		fmt.Fprintln(&buf, "  connections:")
+		for _, c := range conns {
+			fmt.Fprintf(&buf, "    - name: %s\n", yamlize(c.Name().String()))
+			integ, err := r.Client.Integrations().GetByID(ctx, c.IntegrationID())
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(&buf, "      integration: %s\n", yamlize(integ.UniqueName().String()))
+		}
+	}
+
+	fmt.Fprintln(&buf, "  triggers:")
 
 	for _, t := range triggers {
-		fmt.Printf("    - name: %s\n", t.Name().String())
-		fmt.Printf("      event_type: %s\n", t.EventType())
-		fmt.Printf("      call: %s\n", t.CodeLocation().CanonicalString())
+		fmt.Fprintf(&buf, "    - name: %s\n", t.Name().String())
+		fmt.Fprintf(&buf, "      call: %s\n", t.CodeLocation().CanonicalString())
+		if filter := t.Filter(); filter != "" {
+			fmt.Fprintf(&buf, "      filter: %s\n", filter)
+		}
+		if etype := t.EventType(); etype != "" {
+			fmt.Fprintf(&buf, "      event_type: %s\n", etype)
+		}
+
 		switch t.SourceType() {
 		case sdktypes.TriggerSourceTypeWebhook:
-			fmt.Println("      webhook: {}")
+			fmt.Fprintln(&buf, "      webhook: {}")
 			// TODO: More types
+		case sdktypes.TriggerSourceTypeSchedule:
+			fmt.Fprintf(&buf, "      schedule: %s\n", yamlize(t.Schedule()))
+		case sdktypes.TriggerSourceTypeConnection:
+			conn, err := r.Client.Connections().Get(ctx, t.ConnectionID())
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(&buf, "      connection: %s\n", yamlize(conn.Name().String()))
 		}
 	}
 
@@ -68,7 +107,8 @@ func export(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	fmt.Println("  vars:")
+	// Collect vars first, print only if there are some
+	varsMap := make(map[string]string)
 	for _, env := range envs {
 		sid, err := sdktypes.Strict(sdktypes.ParseVarScopeID(env.ID().String()))
 		if err != nil {
@@ -79,10 +119,24 @@ func export(cmd *cobra.Command, args []string) error {
 			if v.IsSecret() {
 				continue
 			}
-			fmt.Printf("    - name: %s\n", v.Name().String())
-			fmt.Printf("      value: %s\n", v.Value())
+			varsMap[v.Name().String()] = v.Value()
 		}
 	}
 
+	if len(varsMap) > 0 {
+		fmt.Fprintln(&buf, "  vars:")
+		for n, v := range varsMap {
+			fmt.Fprintf(&buf, "    - name: %s\n", n)
+			fmt.Fprintf(&buf, "      value: %s\n", yamlize(v))
+		}
+	}
+
+	fmt.Println(buf.String())
 	return nil
+}
+
+func yamlize(v string) string {
+	data, _ := yaml.Marshal(v)
+	// Trim newline added by yaml.Marshal
+	return string(data[:len(data)-1])
 }
