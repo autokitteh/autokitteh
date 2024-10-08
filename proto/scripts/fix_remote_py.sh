25ade79412bc24f67c1248f77ad433096913caa0
Miki Tebeka <miki@autokitteh.com>
2024-10-08 14:18:25 +0300

    bug(python): Generate gRPC Python bindings
    Fixes: ENG-1664

--- proto/scripts/fix_remote_py.sh
+++ proto/scripts/fix_remote_py.sh
@@ -3,3 +3,7 @@
 # Make directories in pb importable
 find ../runtimes/pythonrt/runner/pb -type d -exec touch {}/__init__.py \;
 
+# Change import path in grpc
+sed -i \
+	's/from autokitteh.remote.v1 import remote_pb2/from . import remote_pb2/' \
+	../runtimes/pythonrt/runner/pb/autokitteh/remote/v1/remote_pb2_grpc.py
