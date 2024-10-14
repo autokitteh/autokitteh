49b93a6031ba65447f02e9048c9498d49a3fd2f7
Itay Donanhirsh <itay@autokitteh.com>
2024-10-14 16:10:15 -0700

    work

--- proto/scripts/fix_remote_py.sh
+++ proto/scripts/fix_remote_py.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
+out_dir="../runtimes/pythonrt/runner/pb"
+
 # Make directories in pb importable
-find ../runtimes/pythonrt/runner/pb -type d -exec touch {}/__init__.py \;
+find ${out_dir} -type d -exec touch {}/__init__.py \;
 
 # Change import path in grpc
-sed 's/from autokitteh.remote.v1 import remote_pb2/from . import remote_pb2/' ../runtimes/pythonrt/runner/pb/autokitteh/remote/v1/remote_pb2_grpc.py > /tmp/fixed.py
-mv /tmp/fixed.py ../runtimes/pythonrt/runner/pb/autokitteh/remote/v1/remote_pb2_grpc.py
+
+replace() {
+    sed "s/$2/$3/" "${out_dir}/$1" > "${out_dir}/$1.new"
+    mv -f "${out_dir}/$1.new" "${out_dir}/$1"
+}
+
+
+replace autokitteh/remote/v1/remote_pb2_grpc.py "from autokitteh.remote.v1 import remote_pb2" "from . import remote_pb2"
+
+replace autokitteh/values/v1/values_pb2.py "from autokitteh." "from pb.autokitteh." 
+replace buf/validate/validate_pb2.py "from buf." "from pb.buf."
+replace autokitteh/module/v1/module_pb2.py "from buf." "from pb.buf."
+replace autokitteh/values/v1/values_pb2.py "from buf." "from pb.buf."
