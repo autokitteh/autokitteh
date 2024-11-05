a01dea16e1b2308ffbe9558e00294cff663f9eab
Itay Donanhirsh <itay@autokitteh.com>
2024-11-05 09:18:29 -0800

    feat(python): wrap/unwrap custom values (#819)

--- runtimes/pythonrt/runner/values.py
+++ runtimes/pythonrt/runner/values.py
@@ -6,13 +6,66 @@ Wraps and unwraps autokitteh values.
 import pb.autokitteh.values.v1.values_pb2 as pb
 from collections import namedtuple
 from datetime import datetime, timedelta, UTC
+import pickle
+from typing import Callable
 
 from google.protobuf.duration_pb2 import Duration
 from google.protobuf.timestamp_pb2 import Timestamp
 
+Nothing = pb.Value(nothing=pb.Nothing())
 
-def wrap(v: any) -> pb.Value:
+
+class Wrapper:
+    def __init__(self, executor_id: str):
+        self.executor_id = executor_id
+
+    def wrap_in_custom(self, v: any) -> pb.Value:
+        """Wrap a custom value.
+
+        This is a convenience function to wrap a custom value.
+        """
+
+        return pb.Value(
+            custom=pb.Custom(
+                executor_id=self.executor_id,
+                data=pickle.dumps(v, protocol=0),
+                value=wrap(v, unhandled=lambda _: "<unpresentable>"),
+            ),
+        )
+
+    def wrap(self, v: any) -> pb.Value:
+        """Wrap a python value into an autokitteh value.
+
+        If a type is not supported, it is pickled and wrapped as a Custom value.
+        The present_as function is called with the value that is unsupported to get
+        the presentable value for it.
+        """
+
+        return wrap(v, unhandled=self.wrap_in_custom)
+
+    def unwrap(self, v: pb.Value) -> any:
+        """Unwrap an autokitteh value into a python value.
+
+        If the value is a Custom value, the custom function is called with the value.
+        """
+
+        def custom(v: pb.Value):
+            xid = v.custom.executor_id
+            if v.custom.executor_id != self.executor_id:
+                raise TypeError(
+                    f"can not unwrap custom value from unknown executor {xid} != {self.executor_id}"
+                )
+
+            return pickle.loads(v.custom.data)
+
+        return unwrap(v, custom)
+
+
+def wrap(v: any, unhandled: Callable[[any], pb.Value] = None) -> pb.Value:
     """Wrap a python value into an autokitteh value.
+
+    If a type is not supported, the unhandled function is called with the value if supplied.
+    If not supplied, TypeError is raised.
 
     Tuples are considered as lists.
     Classes with __slots__ or __dict__ are wrapped as structs.
@@ -35,19 +88,22 @@ def wrap(v: any) -> pb.Value:
         if hasattr(type(v), "_fields"):  # namedtuple
             return pb.Value(
                 struct=pb.Struct(
-                    ctor=wrap(v.__class__.__name__),
+                    ctor=wrap(v.__class__.__name__, unhandled),
                     fields={f: wrap(getattr(v, f)) for f in v._fields},
                 )
             )
-        return pb.Value(list=pb.List(vs=[wrap(x) for x in v]))
+        return pb.Value(list=pb.List(vs=[wrap(x, unhandled) for x in v]))
     if isinstance(v, list):
-        return pb.Value(list=pb.List(vs=[wrap(x) for x in v]))
+        return pb.Value(list=pb.List(vs=[wrap(x, unhandled) for x in v]))
     if isinstance(v, set):
-        return pb.Value(set=pb.Set(vs=[wrap(x) for x in v]))
+        return pb.Value(set=pb.Set(vs=[wrap(x, unhandled) for x in v]))
     if isinstance(v, dict):
         return pb.Value(
             dict=pb.Dict(
-                items=[pb.Dict.Item(k=wrap(k), v=wrap(v)) for k, v in v.items()]
+                items=[
+                    pb.Dict.Item(k=wrap(k, unhandled), v=wrap(v, unhandled))
+                    for k, v in v.items()
+                ]
             )
         )
     if isinstance(v, bytes):
@@ -65,23 +121,26 @@ def wrap(v: any) -> pb.Value:
     if hasattr(v, "__dict__"):
         return pb.Value(
             struct=pb.Struct(
-                ctor=wrap(v.__class__.__name__),
-                fields={k: wrap(v) for k, v in v.__dict__.items()},
+                ctor=wrap(v.__class__.__name__, unhandled),
+                fields={k: wrap(v, unhandled) for k, v in v.__dict__.items()},
             )
         )
 
     if hasattr(v, "__slots__"):
         return pb.Value(
             struct=pb.Struct(
-                ctor=wrap(v.__class__.__name__),
-                fields={k: wrap(getattr(v, k)) for k in v.__slots__},
+                ctor=wrap(v.__class__.__name__, unhandled),
+                fields={k: wrap(getattr(v, k), unhandled) for k in v.__slots__},
             )
         )
+
+    if unhandled:
+        return unhandled(v)
 
     raise TypeError(f"unsupported type: {type(v)}")
 
 
-def unwrap(v: pb.Value) -> any:
+def unwrap(v: pb.Value, custom: Callable[[pb.Value], any] = None) -> any:
     """Unwrap an autokitteh value into a python value.
 
     Note that wrap and unwrap are guaranteed to be symmetric.
@@ -107,19 +166,24 @@ def unwrap(v: pb.Value) -> any:
     if v.HasField("boolean"):
         return v.boolean.v
     if v.HasField("list"):
-        return [unwrap(x) for x in v.list.vs]
+        return [unwrap(x, custom) for x in v.list.vs]
     if v.HasField("set"):
-        return set([unwrap(x) for x in v.set.vs])
+        return set([unwrap(x, custom) for x in v.set.vs])
     if v.HasField("dict"):
-        return {unwrap(x.k): unwrap(x.v) for x in v.dict.items}
+        return {unwrap(x.k, custom): unwrap(x.v, custom) for x in v.dict.items}
     if v.HasField("bytes"):
         return v.bytes.v
     if v.HasField("struct"):
-        tpl = namedtuple(str(unwrap(v.struct.ctor)), v.struct.fields.keys())
-        return tpl(*[unwrap(x) for x in v.struct.fields.values()])
+        tpl = namedtuple(str(unwrap(v.struct.ctor, custom)), v.struct.fields.keys())
+        return tpl(*[unwrap(x, custom) for x in v.struct.fields.values()])
     if v.HasField("time"):
         return v.time.v.ToDatetime(UTC)
     if v.HasField("duration"):
         return v.duration.v.ToTimedelta()
+    if v.HasField("function"):
+        return v.function
+    if v.HasField("custom"):
+        if v.custom:
+            return custom(v)
 
     raise TypeError(f"unsupported type: {v}")
