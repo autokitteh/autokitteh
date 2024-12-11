dfceb69b9f37f0f1755c26971673624c4f3c86bf
Mario Rohana <mario@autokitteh.com>
2024-12-11 10:26:10 +0200

    slack event handler fixes

--- integrations/slack/webhooks/bot_events.go
+++ integrations/slack/webhooks/bot_events.go
@@ -18,10 +18,10 @@ const (
 type BotEventHandler = func(*zap.Logger, http.ResponseWriter, []byte, *events.Callback) any
 
 var BotEventHandlers = map[string]BotEventHandler{
-	"app_mention": events.AppMentionHandler,
+	"app_home_opened": events.AppHomeOpenHandler,
+	"app_mention":     events.AppMentionHandler,
 	// TODO: app_rate_limit
 	// TODO: app_uninstalled
-	"app_home_opened": events.AppHomeOpenHandler,
 
 	"channel_archive": events.ChannelGroupHandler,
 	"channel_created": events.ChannelCreatedHandler,
