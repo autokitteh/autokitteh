2135045cc6375cf0d346003836dafb8f35b9c635
Mario Rohana <104531719+mario99logic@users.noreply.github.com>
2024-12-15 10:42:02 +0200

    feat: add event handler for "app_home_opened" Slack (#873)
    Added a new AppHomeOpenHandler to process app_home_opened events from
    Slack. This handler validates the event payload and ignores
    self-triggered events.
    
    Refs: INT-93
    
    ---------
    
    Co-authored-by: Miki Tebeka <miki@autokitteh.com>
    Co-authored-by: Daniel Abraham <daniel@autokitteh.com>

--- integrations/slack/webhooks/bot_events.go
+++ integrations/slack/webhooks/bot_events.go
@@ -18,7 +18,8 @@ const (
 type BotEventHandler = func(*zap.Logger, http.ResponseWriter, []byte, *events.Callback) any
 
 var BotEventHandlers = map[string]BotEventHandler{
-	"app_mention": events.AppMentionHandler,
+	"app_home_opened": events.AppHomeOpenedHandler,
+	"app_mention":     events.AppMentionHandler,
 	// TODO: app_rate_limit
 	// TODO: app_uninstalled
 
