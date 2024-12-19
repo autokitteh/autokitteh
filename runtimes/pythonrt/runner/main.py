860e7e62e9a0a259a4477d7ffd0392028ba4544c
Miki Tebeka <miki@autokitteh.com>
2024-12-19 14:28:39 +0200

    feat(python): Add stack information to gRPC errors (#874)
    Fixes ENG-1895

--- runtimes/pythonrt/runner/main.py
+++ runtimes/pythonrt/runner/main.py
@@ -109,6 +109,14 @@ def killIfStartWasntCalled(runner):
         os._exit(1)
 
 
+def abort_with_exception(context, status, err):
+    io = StringIO()
+    print(err, file=io)
+    for line in format_exception(err):
+        io.write(line)
+    context.abort(status, io.getvalue())
+
+
 Call = namedtuple("Call", "fn args kw fut")
 
 
@@ -136,7 +144,7 @@ class Runner(pb.runner_rpc.RunnerService):
         try:
             exports = list(loader.exports(self.code_dir, request.file_name))
         except OSError as err:
-            context.abort(grpc.StatusCode.INVALID_ARGUMENT, str(err))
+            abort_with_exception(context, grpc.StatusCode.INVALID_ARGUMENT, err)
 
         return pb.runner.ExportsResponse(exports=exports)
 
@@ -253,7 +261,7 @@ class Runner(pb.runner_rpc.RunnerService):
             result = pickle.loads(request.result.custom.data)
         except Exception as err:
             log.exception(f"can't decode data: pickle: {err}")
-            context.abort(grpc.StatusCode.INTERNAL, f"result pickle: {err}")
+            abort_with_exception(context, grpc.StatusCode.INTERNAL, err)
 
         with self.lock:
             call = self.activity_calls.pop() if self.activity_calls else None
