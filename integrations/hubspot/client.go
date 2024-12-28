a8ab155643e525c54725747c73d7fd9e68b20757
Daniel Abraham <daniel@autokitteh.com>
2024-12-27 17:11:43 -0800

    add missing error log

--- integrations/hubspot/client.go
+++ integrations/hubspot/client.go
@@ -56,6 +56,7 @@ func connStatus(i *integration) sdkintegrations.OptFn {
 
 		vs, err := i.vars.Get(ctx, sdktypes.NewVarScopeID(cid))
 		if err != nil {
+			zap.L().Error("failed to read connection vars", zap.String("connection_id", cid.String()), zap.Error(err))
 			return sdktypes.InvalidStatus, err
 		}
 
