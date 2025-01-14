52583b3d79bfb1d37a6d78fe0ca21521c1bd58cd
Miki Tebeka <miki@autokitteh.com>
2025-01-14 13:00:28 +0200

    CI python path

--- cmd/ak/cmd/sessions/test_test.go
+++ cmd/ak/cmd/sessions/test_test.go
@@ -12,6 +12,10 @@ var normCases = []struct {
 }{
 	{"", ""},
 	{"/usr/lib/python3.12/concurrent/futures/_base.py", "py/concurrent/futures/_base.py"},
+	{
+		`File "/opt/hostedtoolcache/Python/3.12.8/x64/lib/python3.12/concurrent/futures/_base.py", line 401, in __get_result`,
+		`py/concurrent/futures/_base.py", line 401, in __get_result`,
+	},
 	{"/tmp/ak-user-2767870919/main.py:6.1,main", "main.py:6.1,main"},
 	{"/tmp/ak-runner-2767870918/main.py:6.1,main", ""},
 	{"runner/main.py:6.1,main, in _call", ""},
