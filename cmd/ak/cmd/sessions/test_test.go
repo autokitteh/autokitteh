7b82b9ebace5f028ebfbaf62a4fa0ac98f252b68
Miki Tebeka <miki@autokitteh.com>
2025-01-19 15:16:24 +0200

    fix test

--- cmd/ak/cmd/sessions/test_test.go
+++ cmd/ak/cmd/sessions/test_test.go
@@ -16,8 +16,8 @@ var normCases = []struct {
 		`py-lib/concurrent/futures/_base.py, line XXX, in __get_result`,
 	},
 	{"/tmp/ak-user-2767870919/main.py:6.1,main", "main.py:6.1,main"},
-	{"/tmp/ak-runner-2767870918/main.py:6.1,main", "ak-runner"},
-	{"runner/main.py:6.1,main, in _call", ""},
+	{"/tmp/ak-runner-2767870918/main.py:6.1,main", "   ak-runner"},
+	{"runner/main.py:6.1,main, in _call", "   ak-runner"},
 	{"ERROR: bad token", "ERROR: bad token"},
 }
 
