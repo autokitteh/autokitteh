74ab9f28aa50004e5c397022cb5fcd48b84ac56d
Miki Tebeka <miki@autokitteh.com>
2025-01-19 14:58:49 +0200

    vscode, please save

--- cmd/ak/cmd/sessions/test_test.go
+++ cmd/ak/cmd/sessions/test_test.go
@@ -16,7 +16,7 @@ var normCases = []struct {
 		`py-lib/concurrent/futures/_base.py, line XXX, in __get_result`,
 	},
 	{"/tmp/ak-user-2767870919/main.py:6.1,main", "main.py:6.1,main"},
-	{"/tmp/ak-runner-2767870918/main.py:6.1,main", ""},
+	{"/tmp/ak-runner-2767870918/main.py:6.1,main", "ak-runner"},
 	{"runner/main.py:6.1,main, in _call", ""},
 	{"ERROR: bad token", "ERROR: bad token"},
 }
