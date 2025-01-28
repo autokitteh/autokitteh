21a807a6904e2965991bde3893b4190688c0cfe0
Pasha Fateev <pasha@autokitteh.com>
2025-01-28 14:12:49 -0800

    feat: add google drive event watch renewals (#1015)
    **Testing:** 
    I updated the cron schedule to run every minute and set the expiration
    to 24 hours. This ensures that a watch renewal is triggered every time I
    check.
    
    Result:
    Every minute a new watch is recreated with a later expiration time than
    before.
    ```
    2025-01-27 10:44:00 WARN drive/client.go:179 Google Drive watch channel creation error {"googleApiError": "googleapi: Error 400: Channel id con_01jjmgtqy7exsbmyhdge7xpea2/events not unique, channelIdNotUnique"}
    2025-01-27 10:44:00 INFO drive/client.go:188 Google Drive watch channel already exists - stopping and recreating
    2025-01-27 10:44:01 WARN drive/client.go:197 Google Drive watch channel recreated {"expiration": 1738089840000}
    2025-01-27 10:45:00 WARN drive/client.go:179 Google Drive watch channel creation error {"googleApiError": "googleapi: Error 400: Channel id con_01jjmgtqy7exsbmyhdge7xpea2/events not unique, channelIdNotUnique"}
    2025-01-27 10:45:00 INFO drive/client.go:188 Google Drive watch channel already exists - stopping and recreating
    2025-01-27 10:45:01 WARN drive/client.go:197 Google Drive watch channel recreated {"expiration": 1738089900000}
    2025-01-27 10:46:00 WARN drive/client.go:179 Google Drive watch channel creation error {"googleApiError": "googleapi: Error 400: Channel id con_01jjmgtqy7exsbmyhdge7xpea2/events not unique, channelIdNotUnique"}
    2025-01-27 10:46:00 INFO drive/client.go:188 Google Drive watch channel already exists - stopping and recreating
    2025-01-27 10:46:01 WARN drive/client.go:197 Google Drive watch channel recreated {"expiration": 1738089960000}
    2025-01-27 10:47:00 WARN drive/client.go:179 Google Drive watch channel creation error {"googleApiError": "googleapi: Error 400: Channel id con_01jjmgtqy7exsbmyhdge7xpea2/events not unique, channelIdNotUnique"}
    2025-01-27 10:47:00 INFO drive/client.go:188 Google Drive watch channel already exists - stopping and recreating
    2025-01-27 10:47:01 WARN drive/client.go:197 Google Drive watch channel recreated {"expiration": 1738090020000}
    ```
    -----
    INT-215 addresses the issue of making an unnecessary check for a channel
    that we know exists.
    
    ---------
    
    Co-authored-by: Daniel Abraham <daniel@autokitteh.com>

--- internal/backend/cron/google.go
+++ internal/backend/cron/google.go
@@ -2,9 +2,18 @@ package cron
 
 import (
 	"context"
+	"errors"
+	"strconv"
+	"time"
 
 	"go.temporal.io/sdk/workflow"
+	"go.uber.org/zap"
 
+	"go.autokitteh.dev/autokitteh/integrations/google/drive"
+	"go.autokitteh.dev/autokitteh/integrations/google/vars"
+	"go.autokitteh.dev/autokitteh/internal/backend/auth/authcontext"
+	"go.autokitteh.dev/autokitteh/internal/backend/temporalclient"
+	"go.autokitteh.dev/autokitteh/sdk/sdkservices"
 	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
 )
 
@@ -15,7 +24,23 @@ func (cr *Cron) renewGoogleCalendarEventWatchesWorkflow(wctx workflow.Context) e
 }
 
 func (cr *Cron) renewGoogleDriveEventWatchesWorkflow(wctx workflow.Context) error {
-	return nil // TODO(INT-184): Implement.
+	actx := temporalclient.WithActivityOptions(wctx, taskQueueName, cr.cfg.Activity)
+
+	var cids []sdktypes.ConnectionID
+	err := workflow.ExecuteActivity(actx, cr.listGoogleDriveConnectionsActivity).Get(wctx, &cids)
+	if err != nil {
+		return err
+	}
+
+	errs := make([]error, 0)
+	for _, cid := range cids {
+		err := workflow.ExecuteActivity(actx, cr.renewGoogleDriveEventWatchActivity, cid).Get(wctx, nil)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	return errors.Join(errs...)
 }
 
 func (cr *Cron) listGoogleCalendarConnectionsActivity(ctx context.Context) ([]sdktypes.ConnectionID, error) {
@@ -23,7 +48,50 @@ func (cr *Cron) listGoogleCalendarConnectionsActivity(ctx context.Context) ([]sd
 }
 
 func (cr *Cron) listGoogleDriveConnectionsActivity(ctx context.Context) ([]sdktypes.ConnectionID, error) {
-	return nil, nil // INT-184: Implement.
+	ctx = authcontext.SetAuthnSystemUser(ctx)
+
+	// Enumerate all Google Drive connections (there's no single connection var value
+	// that we're looking for, so we can't use "ct.vars.FindConnectionIDs").
+	cs, err := cr.connections.List(ctx, sdkservices.ListConnectionsFilter{
+		IntegrationID: drive.IntegrationID,
+	})
+	if err != nil {
+		cr.logger.Error("failed to list Google Drive connections for event watch renewal", zap.Error(err))
+		return nil, err
+	}
+
+	cids := make([]sdktypes.ConnectionID, 0)
+	for _, c := range cs {
+		cid := c.ID()
+		if cr.checkGoogleDriveEventWatch(ctx, cid) {
+			cids = append(cids, cid)
+		}
+	}
+
+	return cids, nil
+}
+
+func (cr *Cron) checkGoogleDriveEventWatch(ctx context.Context, cid sdktypes.ConnectionID) bool {
+	l := cr.logger.With(zap.String("connection_id", cid.String()))
+
+	vs, err := cr.vars.Get(ctx, sdktypes.NewVarScopeID(cid), vars.DriveEventsWatchExp)
+	if err != nil {
+		l.Error("failed to get Google Drive connection vars for event watch renewal", zap.Error(err))
+		return false
+	}
+
+	e := vs.GetValue(vars.DriveEventsWatchExp)
+	threeDaysFromNow := time.Now().UTC().AddDate(0, 0, 3)
+
+	timestamp, err := strconv.ParseInt(e, 10, 64)
+	if err != nil {
+		l.Error("invalid Google Drive event watch expiration timestamp",
+			zap.String("expiration", e), zap.Error(err),
+		)
+		return false
+	}
+
+	return time.Unix(timestamp, 0).UTC().Before(threeDaysFromNow)
 }
 
 func (cr *Cron) renewGoogleCalendarEventWatchActivity(ctx context.Context, cid sdktypes.ConnectionID) error {
@@ -31,5 +99,14 @@ func (cr *Cron) renewGoogleCalendarEventWatchActivity(ctx context.Context, cid s
 }
 
 func (cr *Cron) renewGoogleDriveEventWatchActivity(ctx context.Context, cid sdktypes.ConnectionID) error {
-	return nil // TODO(INT-184): Implement.
+	l := cr.logger.With(zap.String("connection_id", cid.String()))
+	ctx = authcontext.SetAuthnSystemUser(ctx)
+
+	err := drive.UpdateWatches(ctx, cr.vars, cid)
+	if err != nil {
+		l.Error("failed to renew Google Drive event watch", zap.Error(err))
+		return err
+	}
+
+	return nil
 }
