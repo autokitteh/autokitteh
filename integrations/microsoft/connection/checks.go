09a9354442bfcf1bbef122aa6a1451c2dd91ac45
Daniel Abraham <daniel@autokitteh.com>
2025-02-01 22:46:09 -0800

    feat(microsoft): support more auth types + MS Teams fixes (#1033)
    1. Add support for "daemon apps" (apps with their own identity, like
    private OAuth apps but without user-delegated permissions, i.e. bots)
    2. Connection test for daemon apps (there's no "me" user identity)
    3. Add OAuth scopes for MS Teams
    4. `"isCustomOauth"`: add logic to detect private MS apps
    5. Python SDK: add MS Team initialization function, support for private
    OAuth/daemon apps, and certificate-based private apps
    
    Refs: INT-202, INT-227

--- integrations/microsoft/connection/checks.go
+++ integrations/microsoft/connection/checks.go
@@ -5,9 +5,12 @@ package connection
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"golang.org/x/oauth2"
 
@@ -16,8 +19,6 @@ import (
 	"go.autokitteh.dev/autokitteh/sdk/sdkservices"
 	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
 )
-
-var AuthTypeVar = sdktypes.NewSymbol("auth_type")
 
 // Status checks the connection's initialization status (is it
 // initialized? what type of authentication is configured?). This
@@ -39,6 +40,8 @@ func Status(v sdkservices.Vars) sdkintegrations.OptFn {
 			return sdktypes.NewStatus(sdktypes.StatusCodeWarning, "Init required"), nil
 		case integrations.OAuthDefault, integrations.OAuthPrivate:
 			return sdktypes.NewStatus(sdktypes.StatusCodeOK, "Using OAuth 2.0"), nil
+		case integrations.DaemonApp:
+			return sdktypes.NewStatus(sdktypes.StatusCodeOK, "Using daemon app"), nil
 		default:
 			return sdktypes.NewStatus(sdktypes.StatusCodeError, "Bad auth type"), nil
 		}
@@ -63,14 +66,15 @@ func Test(v sdkservices.Vars, o sdkservices.OAuth) sdkintegrations.OptFn {
 		case "":
 			return sdktypes.NewStatus(sdktypes.StatusCodeWarning, "Init required"), nil
 		case integrations.OAuthDefault, integrations.OAuthPrivate:
-			// Don't return, continue to do the check below.
+			if _, err = GetUserInfo(ctx, oauthToken(ctx, vs, o)); err != nil {
+				return sdktypes.NewStatus(sdktypes.StatusCodeError, err.Error()), nil
+			}
+		case integrations.DaemonApp:
+			if err = GetDaemonToken(ctx, vs); err != nil {
+				return sdktypes.NewStatus(sdktypes.StatusCodeError, err.Error()), nil
+			}
 		default:
 			return sdktypes.NewStatus(sdktypes.StatusCodeError, "Bad auth type"), nil
-		}
-
-		// Load and attempt to use the OAuth token.
-		if _, err = GetUserInfo(ctx, oauthToken(ctx, vs, o)); err != nil {
-			return sdktypes.NewStatus(sdktypes.StatusCodeError, err.Error()), nil
 		}
 
 		return sdktypes.NewStatus(sdktypes.StatusCodeOK, ""), nil
@@ -81,9 +85,9 @@ func Test(v sdkservices.Vars, o sdkservices.OAuth) sdkintegrations.OptFn {
 // unless it's stale, in which case it will be refreshed first.
 func oauthToken(ctx context.Context, vs sdktypes.Vars, o sdkservices.OAuth) *oauth2.Token {
 	t1 := &oauth2.Token{
-		AccessToken:  vs.GetValueByString("oauth_access_token"),
-		RefreshToken: vs.GetValueByString("oauth_refresh_token"),
-		TokenType:    vs.GetValueByString("oauth_token_type"),
+		AccessToken:  vs.GetValue(oauthAccessTokenVar),
+		RefreshToken: vs.GetValue(oauthRefreshTokenVar),
+		TokenType:    vs.GetValue(oauthTokenTypeVar),
 	}
 	if t1.Valid() {
 		return t1
@@ -102,24 +106,11 @@ func oauthToken(ctx context.Context, vs sdktypes.Vars, o sdkservices.OAuth) *oau
 	return t2
 }
 
-// UserInfo contains user profile details from Microsoft Graph
-// (based on: https://learn.microsoft.com/en-us/graph/api/user-get).
-type UserInfo struct {
-	PrincipalName string `json:"userPrincipalName" var:"principal_name"`
-	ID            string `json:"id" var:"id"`
-	DisplayName   string `json:"displayName" var:"display_name"`
-	Surname       string `json:"surname" var:"surname"`
-	GivenName     string `json:"givenName" var:"given_name"`
-	Language      string `json:"preferredLanguage" var:"language"`
-	Mail          string `json:"mail" var:"mail"`
-	MobilePhone   string `json:"mobilePhone" var:"mobile_phone"`
-}
-
 // GetUserInfo returns the authenticated user's profile from Microsoft
 // Graph (based on: https://learn.microsoft.com/en-us/graph/api/user-get).
 func GetUserInfo(ctx context.Context, t *oauth2.Token) (*UserInfo, error) {
-	url := "https://graph.microsoft.com/v1.0/me"
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+	u := "https://graph.microsoft.com/v1.0/me"
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u, http.NoBody)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
 	}
@@ -131,13 +122,13 @@ func GetUserInfo(ctx context.Context, t *oauth2.Token) (*UserInfo, error) {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request for user info failed: %s", resp.Status)
-	}
-
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read user info response: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request for user info failed: %s (%s)", resp.Status, body)
 	}
 
 	var user UserInfo
@@ -146,4 +137,49 @@ func GetUserInfo(ctx context.Context, t *oauth2.Token) (*UserInfo, error) {
 	}
 
 	return &user, nil
+}
+
+// GetDaemonToken checks whether a Microsoft Graph daemon app is configured correctly
+// (based on: https://learn.microsoft.com/en-us/entra/identity-platform/scenario-daemon-acquire-token).
+func GetDaemonToken(ctx context.Context, vs sdktypes.Vars) error {
+	tenantID := vs.GetValue(privateTenantIDVar)
+	if tenantID == "" {
+		// https://learn.microsoft.com/en-us/answers/questions/1853467/aadsts7000229-the-client-application-is-missing-se
+		return errors.New("missing tenant ID")
+	}
+	u := fmt.Sprintf("https://login.microsoftonline.com/%s/oauth2/v2.0/token", tenantID)
+
+	// TODO(INT-227): Add support for certificate-based authentication.
+	data := url.Values{}
+	data.Set("grant_type", "client_credentials")
+	data.Set("client_id", vs.GetValue(privateClientIDVar))
+	data.Set("client_secret", vs.GetValue(privateClientSecretVar))
+	data.Set("scope", "https://graph.microsoft.com/.default")
+
+	if data.Get("client_id") == "" || data.Get("client_secret") == "" {
+		return errors.New("missing required connection variables")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u, strings.NewReader(data.Encode()))
+	if err != nil {
+		return fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("request for token failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read token response: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("request for token failed: %s (%s)", resp.Status, body)
+	}
+
+	return nil
 }
