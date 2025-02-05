39341738f82164f83845ba273424e224c7e97ab9
Pasha Fateev <pasha@autokitteh.com>
2025-02-04 16:56:06 -0800

    Update integrations/height/vars.go
    Co-authored-by: Daniel Abraham <daniel@autokitteh.com>

--- integrations/height/vars.go
+++ integrations/height/vars.go
@@ -27,8 +27,7 @@ func newOAuthData(t *oauth2.Token) oauthData {
 	}
 }
 
-// privateOAuth contains the user-provided details of
-// a private Height OAuth 2.0 app.
+// privateOAuth contains the user-provided details of a private OAuth 2.0 app.
 type privateOAuth struct {
 	ClientID     string `var:"private_client_id"`
 	ClientSecret string `var:"private_client_secret,secret"`
