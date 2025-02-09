d4df248ba5661eb347d51fa7ba352b2360a5092c
Daniel Abraham <daniel@autokitteh.com>
2025-02-08 18:13:49 -0800

    refactor(integrations): webhook handlers (#1055)
    New reusable functions to register UI/save/OAuth webhook handlers in
    `integrations/common/server.go` - mostly for new-style integrations, but
    not just.
    
    Most of the PR is tweaks related to that (using these new functions), or
    cosmetic tweaks (e.g. rename `muxes *muxes.Muxes` variable to `m` to be
    more idiomatic about Go var naming).

--- integrations/auth0/server.go
+++ integrations/auth0/server.go
@@ -1,38 +1,20 @@
 package auth0
 
 import (
-	"net/http"
-
 	"go.uber.org/zap"
 
+	"go.autokitteh.dev/autokitteh/integrations/common"
 	"go.autokitteh.dev/autokitteh/internal/backend/muxes"
 	"go.autokitteh.dev/autokitteh/sdk/sdkservices"
 	"go.autokitteh.dev/autokitteh/web/static"
 )
 
-const (
-	// oauthPath is the URL path for our handler to save
-	// new OAuth-based connections.
-	oauthPath = "/auth0/oauth"
-
-	// savePath is the URL path for our handler to save authentication credentials.
-	// It acts as a passthrough for the OAuth flow.
-	savePath = "/auth0/save"
-)
-
 // Start initializes all the HTTP handlers of the Auth0 integration.
 // This includes connection UIs, initialization webhooks, and event webhooks.
-func Start(l *zap.Logger, muxes *muxes.Muxes, vars sdkservices.Vars) {
-	// Connection UI.
-	uiPath := "GET " + desc.ConnectionURL().Path + "/"
-	muxes.NoAuth.Handle(uiPath, http.FileServer(http.FS(static.Auth0WebContent)))
+func Start(l *zap.Logger, m *muxes.Muxes, vars sdkservices.Vars) {
+	common.ServeStaticUI(m, desc, static.Auth0WebContent)
 
-	// Init webhooks save connection vars (via "c.Finalize" calls), so they need
-	// to have an authenticated user context, so the DB layer won't reject them.
-	// For this purpose, init webhooks are managed by the "auth" mux, which passes
-	// through AutoKitteh's auth middleware to extract the user ID from a cookie.
 	h := NewHTTPHandler(l, vars)
-	muxes.Auth.HandleFunc("GET "+oauthPath, h.handleOAuth)
-	muxes.Auth.HandleFunc("GET "+savePath, h.handleSave)  // Web UI passthrough for OAuth.
-	muxes.Auth.HandleFunc("POST "+savePath, h.handleSave) // Internal UI passthrough for OAuth.
+	common.RegisterSaveHandler(m, desc, h.handleSave)
+	common.RegisterOAuthHandler(m, desc, h.handleOAuth)
 }
---
+++ integrations/common/server.go
@@ -0,0 +1,38 @@
+package common
+
+import (
+	"embed"
+	"fmt"
+	"net/http"
+
+	"go.autokitteh.dev/autokitteh/internal/backend/muxes"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+)
+
+// ServeStaticUI registers an integration's static web content to
+// AutoKitteh's internal user-authenticated HTTP server. User auth
+// isn't actually required for serving these files, but it's impossible
+// to use them (i.e. to initialize a connection) without authentication.
+func ServeStaticUI(m *muxes.Muxes, i sdktypes.Integration, fs embed.FS) {
+	pattern := fmt.Sprintf("GET %s/", i.ConnectionURL().Path)
+	m.Auth.Handle(pattern, http.FileServer(http.FS(fs)))
+}
+
+// RegisterSaveHandler registers a webhook to handle saving connection variables.
+// This is always the first step in initializing a connection, regardless of its
+// auth type. It's also the last step for non-OAuth authentication types. The
+// handler function requires an authenticated user context for database access.
+func RegisterSaveHandler(m *muxes.Muxes, i sdktypes.Integration, h http.HandlerFunc) {
+	pattern := fmt.Sprintf(" %s/save", i.ConnectionURL().Path)
+	m.Auth.HandleFunc(http.MethodGet+pattern, h)
+	m.Auth.HandleFunc(http.MethodPost+pattern, h)
+}
+
+// RegisterOAuthHandler registers a webhook to handle the last step in a 3-legged
+// OAuth 2.0 flow. This is the only step that requires an authenticated user context
+// for database access. The handler function receives an incoming redirect request
+// from AutoKitteh's generic OAuth service, which contains an OAuth token (if the
+// OAuth flow was successful) and form parameters for debugging and validation.
+func RegisterOAuthHandler(m *muxes.Muxes, i sdktypes.Integration, h http.HandlerFunc) {
+	m.Auth.HandleFunc(fmt.Sprintf("GET %s/oauth", i.ConnectionURL().Path), h)
+}
