08a12f028dc9e4f7fc680dcbb080221f8fc6353a
Daniel Abraham <daniel@autokitteh.com>
2025-02-10 08:52:50 -0800

    feat(zoom): support server-to-server auth (#1059)
    Tested only the saving of credentials, not their usage - that should be
    done in INT-247.
    
    Refs: INT-241

--- integrations/zoom/vars.go
+++ integrations/zoom/vars.go
@@ -27,8 +27,11 @@ func newOAuthData(t *oauth2.Token) oauthData {
 	}
 }
 
-// privateOAuth contains the user-provided details of a private Zoom OAuth 2.0 app.
-type privateOAuth struct {
+// privateApp contains the user-provided details of a private
+// Zoom OAuth 2.0 app or Service-to-Service internal app.
+type privateApp struct {
+	AccountID    string `var:"private_account_id"`
 	ClientID     string `var:"private_client_id"`
 	ClientSecret string `var:"private_client_secret,secret"`
+	SecretToken  string `var:"private_secret_token,secret"`
 }
