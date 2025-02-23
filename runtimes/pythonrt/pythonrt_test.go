a63e0673509c1e921bbae427dcb2055d2b2eea72
Itay Donanhirsh <itay@autokitteh.com>
2025-02-23 05:47:32 +0100

    fix: broken python test in main (#1112)

--- runtimes/pythonrt/pythonrt_test.go
+++ runtimes/pythonrt/pythonrt_test.go
@@ -243,7 +243,9 @@ func Test_pySvc_Run(t *testing.T) {
 		"bytes": sdktypes.NewBytesValue([]byte(`{"user": "joe", "id": 7}`)),
 	})
 	kwargs := map[string]sdktypes.Value{
-		"body": body,
+		"data": sdktypes.NewDictValueFromStringMap(map[string]sdktypes.Value{
+			"body": body,
+		}),
 	}
 	_, err = run.Call(ctx, fn, nil, kwargs)
 	require.NoError(t, err, "call")
