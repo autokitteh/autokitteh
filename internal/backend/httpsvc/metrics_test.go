5273498f91c211bbd56a308e4567a77ae0bbfa12
Daniel Abraham <daniel@autokitteh.com>
2025-02-23 10:04:15 -0800

    use name instead of index

--- internal/backend/httpsvc/metrics_test.go
+++ internal/backend/httpsvc/metrics_test.go
@@ -1,37 +1,40 @@
 package httpsvc
 
 import (
-	"strconv"
 	"testing"
 )
 
 func TestGetMetricNameFromPath(t *testing.T) {
 	tests := []struct {
-		in, out string
+		name, in, out string
 	}{
 		{
-			"irrelevant_path", "",
+			"irrelevant_path", "irrelevant_path", "",
 		},
 		{
+			"v1-projects",
 			"/autokitteh.projects.v1.ProjectsService/Create",
 			"projects.v1.create",
 		},
 		{
+			"v2-projects",
 			"/autokitteh.projects.v2.ProjectsService/Create",
 			"projects.v2.create",
 		},
 		{
+			"v2-other-projects",
 			"/autokitteh.projects.v2.OtherProjectsService/Create",
 			"projects_otherprojects.v2.create",
 		},
 		{
+			"v1-other-projects",
 			"/autokitteh.projects.v1.OtherProjectsService/Create",
 			"projects_otherprojects.v1.create",
 		},
 	}
 
-	for i, tt := range tests {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			if got := getMetricNameFromPath(tt.in); got != tt.out {
 				t.Errorf("got %q, want %q", got, tt)
 			}
