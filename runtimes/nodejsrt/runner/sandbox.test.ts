803a995a3b1179ca0112323e211603e9901b5cc1
adiludmer <adiludmer@gmail.com>
2025-02-28 17:39:00 +0200

    fix: support obj method call

--- runtimes/nodejsrt/runner/sandbox.test.ts
+++ runtimes/nodejsrt/runner/sandbox.test.ts
@@ -2,10 +2,22 @@ import {Sandbox} from "./sandbox";
 
 test('typescript hooking async', async () => {
     let hookCalled = false
-    const hookFunc = async (...args: any): Promise<string> => {
+    const hookFunc = async (...args: any): Promise<any> => {
         let f = args[0];
-        let f_args = args.slice(1)
-        return await f(...f_args);
+        if (typeof f === "function") {
+            let f_args = args.slice(1)
+            let out = await f(...f_args);
+            console.log(out)
+            return out;
+        }
+        else if (typeof f === "object") {
+            let o = args[0];
+            let m = args[1];
+            let m_args = args.slice(2)
+            let out = await o[m](...m_args);
+            console.log(out)
+            return out;
+        }
     }
 
     const sandbox = new Sandbox("/Users/adiludmer/GolandProjects/autokitteh/runtimes/nodejsrt/runner/test_data/replay-demo", hookFunc);
