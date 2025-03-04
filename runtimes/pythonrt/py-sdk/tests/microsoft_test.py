1c514425a4fc9030a708b67823ee139a6692e333
Miki Tebeka <miki@autokitteh.com>
2025-03-04 11:19:11 +0200

    chore(python): Test py-sdk (#1158)
    Add smoke tests to all modules, check ruff and a CI run.
    
    Bumped discord version since `audioop`  was removed in Python 3.13

---
+++ runtimes/pythonrt/py-sdk/tests/microsoft_test.py
@@ -0,0 +1,5 @@
+# Smoke test, fill with content
+
+
+def test_smoke():
+    from autokitteh import microsoft  # noqa: F401
---
+++ runtimes/pythonrt/py-sdk/tests/openai_test.py
@@ -0,0 +1,5 @@
+# Smoke test, fill with content
+
+
+def test_smoke():
+    from autokitteh import openai  # noqa: F401
