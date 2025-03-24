1dc2c84857e17c4af42764ebb57098e3d74f0ccf
Efi Shtain <efi@autokitteh.com>
2025-03-24 15:18:35 +0200

    support setting non secure cookie

--- internal/backend/auth/authsessions/config.go
+++ internal/backend/auth/authsessions/config.go
@@ -8,10 +8,9 @@ import (
 
 type Config struct {
 	SameSite          http.SameSite
-	CookieKeys        string `koanf:"cookie_keys"` // pairs of hash and block keys.
 	Domain            string `koanf:"ui_domain"`
-	Secure            bool
-	ExpirationMinutes int `koanf:"expiration_minutes"`
+	Secure            bool   `koanf:"secure_cookie"`
+	ExpirationMinutes int    `koanf:"expiration_minutes"`
 }
 
 var Configs = configset.Set[Config]{
@@ -23,7 +22,6 @@ var Configs = configset.Set[Config]{
 	Dev: &Config{
 		Secure:            false,
 		SameSite:          http.SameSiteLaxMode,
-		CookieKeys:        "0000000000000000000000000000000000000000000000000000000000000000,0000000000000000000000000000000000000000000000000000000000000000",
 		ExpirationMinutes: 60 * 24 * 14, // 14 days
 	},
 }
