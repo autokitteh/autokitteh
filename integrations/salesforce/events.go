6cb7da5a9d24e6c0005037cd45e49cb3e9ff720f
Pasha Fateev <pasha@autokitteh.com>
2025-03-31 14:52:55 -0700

    chore: add logging to prevent silent errors + log all events in salesforce (#1218)

--- integrations/salesforce/events.go
+++ integrations/salesforce/events.go
@@ -11,6 +11,7 @@ import (
 func (h handler) dispatchEvent(payload map[string]any, eventType string) {
 	akEvent, err := common.TransformEvent(h.logger, payload, eventType)
 	if err != nil {
+		h.logger.Error("failed to transform Salesforce event", zap.Error(err))
 		return
 	}
 
