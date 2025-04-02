e815c5d1dec60128d4fc418e201a40fe3e812362
Alon Ftecha <alonft@gmail.com>
2025-04-02 16:43:55 +0300

    fix eslint issues

--- runtimes/nodejsrt/nodejs/jest.config.js
+++ runtimes/nodejsrt/nodejs/jest.config.js
@@ -1,9 +1,11 @@
+
+// eslint-disable-next-line no-undef
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    globals: {
-        'ts-jest': {
-            tsconfig: './tsconfig.strict.json',
-        },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.strict.json",
     },
+  },
 };
