a6ff50ecb5c7c52311302cada366f21617e3772e
Alon Ftecha <alonft@gmail.com>
2025-04-08 09:51:04 +0300

    events

--- runtimes/nodejsrt/nodejs/simple-test-build.test.ts
+++
@@ -1,6 +0,0 @@
-import { build } from './runtime/builder/build';
-
-test('test', async () => {
-    await build('./simple-test', './simple-test-build');
-})
-
--- runtimes/nodejsrt/nodejs/simple-test-main-direct.test.ts
+++
@@ -1,6 +0,0 @@
-import { mainDirect } from './dev/direct_execution/main_direct';
-
-test('test', async () => {
-    await mainDirect('testdata/simple-test/runtime',{},'testdata/simple-test/runtime/.ak/runtime/main.ts');
-})
-
