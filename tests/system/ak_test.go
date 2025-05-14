e67633014e3c7348b876cf4405fd5245c3e7e203
Mario Rohana <mario@autokitteh.com>
2025-05-14 16:27:00 +0300

    set time out

--- tests/system/ak_test.go
+++ tests/system/ak_test.go
@@ -18,13 +18,11 @@ profiling, and load/stress testing.
 package systest
 
 import (
-	"context"
 	"embed"
 	"fmt"
 	"io/fs"
 	"strings"
 	"testing"
-	"time"
 
 	"go.autokitteh.dev/autokitteh/internal/kittehs"
 	"go.autokitteh.dev/autokitteh/tests"
@@ -127,9 +125,6 @@ func setUpTest(t *testing.T, akPath string, cfg map[string]any) string {
 }
 
 func runTestSteps(t *testing.T, steps []string, akPath, akAddr string, cfg *testConfig) {
-	ctx, cancel := context.WithTimeout(t.Context(), 3*time.Minute)
-	defer cancel()
-
 	var (
 		actionIndex int
 		ak          *tests.AKResult
@@ -138,11 +133,6 @@ func runTestSteps(t *testing.T, steps []string, akPath, akAddr string, cfg *test
 	)
 
 	for i, step := range steps {
-		if err := ctx.Err(); err != nil {
-			t.Fatalf("Test timed out after 2 minutes")
-			return
-		}
-
 		// Skip empty lines and comments.
 		if step == "" {
 			continue
