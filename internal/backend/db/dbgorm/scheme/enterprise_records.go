7aa62e7a1186dc0ffd75bd1710092214311a1d7a
Efi Shtain <efi@autokitteh.com>
2025-06-15 09:35:22 +0300

    workflow thotteller for enterprise

--- internal/backend/db/dbgorm/scheme/enterprise_records.go
+++ internal/backend/db/dbgorm/scheme/enterprise_records.go
@@ -2,3 +2,25 @@
 // +build enterprise
 
 package scheme
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type WorkerInfo struct {
+	WorkerID        string `gorm:"primaryKey"`
+	ActiveWorkflows int
+	UpdatedAt       time.Time
+}
+
+type WorkflowExecutionRequest struct {
+	SessionID  uuid.UUID `gorm:"primaryKey"`
+	Args       []byte
+	Memo       []byte
+	AcquiredAt *time.Time
+	AcquiredBy *string
+
+	CreatedAt time.Time `gorm:"default:NOW()"`
+}
--- internal/backend/db/dbinterface_enterprise.go
+++ internal/backend/db/dbinterface_enterprise.go
@@ -4,6 +4,9 @@
 package db
 
 import (
+	"context"
+
+	"go.autokitteh.dev/autokitteh/internal/backend/db/dbgorm/scheme"
 	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
 )
 
@@ -15,4 +18,13 @@ type WorkflowExecutionRequest struct {
 
 type DB interface {
 	Shared
+	// WorkerInfo
+	GetWorkerInfo(ctx context.Context, id string) (scheme.WorkerInfo, error)
+	IncActiveWorkflows(ctx context.Context, workerID string) (int, error)
+	DecActiveWorkflows(ctx context.Context, workerID string) (int, error)
+
+	// WorkflowExecutionRequest
+	CreateWorkflowExecutionRequest(ctx context.Context, request WorkflowExecutionRequest) error
+	GetWorkflowExecutionRequests(ctx context.Context, workerID string, maxRequests int) ([]WorkflowExecutionRequest, error)
+	DeleteWorkflowExecutionRequest(ctx context.Context, sessionID sdktypes.SessionID) error
 }
