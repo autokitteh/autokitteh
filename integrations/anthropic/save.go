68b4333437ee393a696d833a94dcf6c489c13593
Mario Rohana <mario@autokitteh.com>
2025-06-16 09:18:23 +0300

    add: anthropic connection

---
+++ integrations/anthropic/save.go
@@ -0,0 +1,45 @@
+package anthropic
+
+import (
+	"net/http"
+
+	"go.uber.org/zap"
+
+	"go.autokitteh.dev/autokitteh/integrations/common"
+	"go.autokitteh.dev/autokitteh/sdk/sdkintegrations"
+	"go.autokitteh.dev/autokitteh/sdk/sdktypes"
+)
+
+var apiKeyVar = sdktypes.NewSymbol("api_key")
+
+// handler is an autokitteh webhook which implements [http.Handler]
+// to save data from web form submissions as connections.
+type handler struct {
+	logger *zap.Logger
+}
+
+func NewHTTPHandler(l *zap.Logger) http.Handler {
+	return handler{logger: l}
+}
+
+// ServeHTTP saves a new autokitteh connection with user-submitted data.
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c, l := sdkintegrations.NewConnectionInit(h.logger, w, r, desc)
+
+	// Check the "Content-Type" header.
+	if common.PostWithoutFormContentType(r) {
+		ct := r.Header.Get(common.HeaderContentType)
+		l.Warn("save connection: unexpected POST content type", zap.String("content_type", ct))
+		c.AbortBadRequest("unexpected content type")
+		return
+	}
+
+	// Read and parse POST request body.
+	if err := r.ParseForm(); err != nil {
+		l.Warn("Failed to parse incoming HTTP request", zap.Error(err))
+		c.AbortBadRequest("form parsing error")
+		return
+	}
+
+	c.Finalize(sdktypes.NewVars().Set(apiKeyVar, r.Form.Get("key"), true))
+}
