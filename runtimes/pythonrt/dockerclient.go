757bed29fcaeaab821a9a4e821e5a108c92180c6
efi shtain <efi@autokitteh.com>
2025-08-17 14:15:28 +0300

    ENG-2238: add auto remove to container (#1372)

--- runtimes/pythonrt/dockerclient.go
+++ runtimes/pythonrt/dockerclient.go
@@ -108,6 +108,7 @@ func (d *dockerClient) StartRunner(ctx context.Context, runnerImage string, sess
 			WorkingDir: "/workflow",
 		},
 		&container.HostConfig{
+			AutoRemove:   true,
 			NetworkMode:  container.NetworkMode(networkName),
 			PortBindings: nat.PortMap{internalRunnerPort: []nat.PortBinding{{HostIP: "127.0.0.1"}}},
 			Tmpfs:        map[string]string{"/tmp": "size=64m"},
