71a5336f56a548c29f54237c5cecb936e3c29485
Efi Shtain <efi@autokitteh.com>
2025-08-24 18:57:55 +0300

    restore auto remove

--- runtimes/pythonrt/dockerclient.go
+++ runtimes/pythonrt/dockerclient.go
@@ -109,7 +109,7 @@ func (d *dockerClient) StartRunner(ctx context.Context, runnerImage string, code
 			WorkingDir: "/workflow",
 		},
 		&container.HostConfig{
-			// AutoRemove:   true,
+			AutoRemove:   true,
 			NetworkMode:  container.NetworkMode(networkName),
 			PortBindings: nat.PortMap{internalRunnerPort: []nat.PortBinding{{HostIP: "127.0.0.1"}}},
 			Tmpfs:        map[string]string{"/tmp": "size=64m"},
