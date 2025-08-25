aec0aa1cd00ad80c7e4a966a4c24da1735fdbe24
Efi Shtain <efi@autokitteh.com>
2025-08-25 13:33:14 +0300

    add container default limits

--- runtimes/pythonrt/runner_manager_docker.go
+++ runtimes/pythonrt/runner_manager_docker.go
@@ -15,9 +15,11 @@ import (
 )
 
 type DockerRuntimeConfig struct {
-	WorkerAddressProvider func() string
-	LogRunnerCode         bool
-	LogBuildCode          bool
+	WorkerAddressProvider  func() string
+	LogRunnerCode          bool
+	LogBuildCode           bool
+	MaxMemoryPerWorkflowMB int64
+	MaxCPUsPerWorkflow     float32
 }
 
 type dockerRunnerManager struct {
@@ -29,7 +31,7 @@ type dockerRunnerManager struct {
 }
 
 func configureDockerRunnerManager(log *zap.Logger, cfg DockerRuntimeConfig) error {
-	dc, err := NewDockerClient(log, cfg.LogRunnerCode, cfg.LogBuildCode)
+	dc, err := NewDockerClient(log, cfg)
 	if err != nil {
 		return err
 	}
