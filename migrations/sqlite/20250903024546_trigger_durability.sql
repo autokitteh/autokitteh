446d519594ba40bff67c9db536274e76299bb43b
Itay Donanhirsh <itay@autokitteh.com>
2025-09-05 17:33:13 -0700

    rehash

--- migrations/sqlite/20250903024546_trigger_durability.sql
+++ migrations/sqlite/20250903024546_trigger_durability.sql
@@ -2,6 +2,8 @@
 -- add column "is_durable" to table: "triggers"
 ALTER TABLE `triggers` ADD COLUMN `is_durable` numeric NULL;
 
+UPDATE `triggers` SET is_durable=TRUE;
+
 -- +goose Down
 -- reverse: add column "is_durable" to table: "triggers"
 ALTER TABLE `triggers` DROP COLUMN `is_durable`;
