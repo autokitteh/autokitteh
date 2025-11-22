42c2a189407429a9a90c6370ae2ede40dd503150
Itay Donanhirsh <itay@autokitteh.com>
2025-11-21 19:43:02 -0800

    work

--- runtimes/pythonrt/py-sdk/autokitteh/store.py
+++ runtimes/pythonrt/py-sdk/autokitteh/store.py
@@ -1,3 +1,4 @@
+import os
 from collections.abc import MutableMapping
 from enum import StrEnum
 from typing import Any
@@ -34,6 +35,13 @@ class Store(MutableMapping):
 
 
 store = Store()
+
+
+def get_store_public_url(trigger_name: str) -> str:
+    url = os.getenv("store_public_url")
+    if not url:
+        raise ValueError("Store public URL not found")
+    return url
 
 
 class Op(StrEnum):
