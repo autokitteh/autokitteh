69bf301a8c2c7d65a136a28538632dae7fe2816c
Efi Shtain <efi@autokitteh.com>
2025-11-24 14:35:12 +0200

    simplify migrations

--- migrations/postgres/enterprise/20251123160316_connection-project-id-nullable.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- modify "connections" table
-ALTER TABLE "connections" ALTER COLUMN "project_id" DROP NOT NULL, ALTER COLUMN "org_id" SET NOT NULL;
-
--- +goose Down
--- reverse: modify "connections" table
-ALTER TABLE "connections" ALTER COLUMN "org_id" DROP NOT NULL, ALTER COLUMN "project_id" SET NOT NULL;
