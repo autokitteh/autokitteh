332060125b2d5a55dc06da623187d009e678c43a
Mario Rohana <mario@autokitteh.com>
2025-11-30 10:23:09 +0200

    add data migration for auth_type

---
+++ migrations/sqlite/20251130075958_migrate_auth_type.sql
@@ -0,0 +1,7 @@
+-- +goose Up
+-- migrate "authType" variable name to "auth_type" in vars table
+UPDATE `vars` SET `name` = 'auth_type' WHERE `name` = 'authType';
+
+-- +goose Down
+-- reverse: migrate "authType" variable name to "auth_type" in vars table
+UPDATE `vars` SET `name` = 'authType' WHERE `name` = 'auth_type';
