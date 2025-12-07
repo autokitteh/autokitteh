36912d5e9b46cb5fe6c1cdb640538bd6670c682b
Mario Rohana <mario@autokitteh.com>
2025-12-07 19:32:49 +0200

    syntax google

--- runtimes/pythonrt/py-sdk/autokitteh/google.py
+++ runtimes/pythonrt/py-sdk/autokitteh/google.py
@@ -278,8 +278,7 @@ def google_creds(integration: str, connection: str, scopes: list[str], **kwargs)
     """
     check_connection_name(connection)
 
-    auth_type = os.getenv(connection + "__auth_type")
-    if auth_type == "oauth":  # User (OAuth 2.0)
+    if os.getenv(connection + "__auth_type") == "oauth": # User (OAuth 2.0)
         return _google_creds_oauth2(integration, connection, scopes)
 
     json_key = os.getenv(connection + "__JSON")  # Service Account (JSON key)
