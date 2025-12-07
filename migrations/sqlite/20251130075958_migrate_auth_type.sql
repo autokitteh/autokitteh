908414a05cd52ab6c52080776a727d321de55f8e
Mario Rohana <mario@autokitteh.com>
2025-12-07 14:32:55 +0200

    fix: auth type duplicate names

--- migrations/sqlite/20251130075958_migrate_auth_type.sql
+++ migrations/sqlite/20251130075958_migrate_auth_type.sql
@@ -1,5 +1,10 @@
 -- +goose Up
 -- migrate "authType" variable name to "auth_type" in vars table
+-- Delete any authType entries where auth_type already exists for the same var_id (keep the newer auth_type)
+DELETE FROM `vars` WHERE `name` = 'authType' AND EXISTS (
+  SELECT 1 FROM `vars` v2 WHERE v2.`name` = 'auth_type' AND v2.`var_id` = `vars`.`var_id`
+);
+-- Update remaining authType to auth_type
 UPDATE `vars` SET `name` = 'auth_type' WHERE `name` = 'authType';
 
 -- +goose Down
